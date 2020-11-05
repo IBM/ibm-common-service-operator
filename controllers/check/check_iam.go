329c712bb1e203ecdd228e7e23316a5f7e968a9b
Jiaming Hu <horis233@gmail.com>
2020-11-05 12:21:40 -0500

    remove iam-policy-controller (#286)

--- controllers/check/check_iam.go
+++ controllers/check/check_iam.go
@@ -32,7 +32,7 @@ import (
 )
 
 var (
-	DeployNames = []string{"ibm-iam-operator", "auth-idp", "auth-pap", "auth-pdp", "iam-policy-controller", "oidcclient-watcher", "secret-watcher"}
+	DeployNames = []string{"ibm-iam-operator", "auth-idp", "auth-pap", "auth-pdp", "oidcclient-watcher", "secret-watcher"}
 	JobNames    = []string{"iam-onboarding", "security-onboarding", "oidc-client-registration"}
 )
 
