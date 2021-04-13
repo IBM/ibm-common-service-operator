aaa48c7632975af05d3cfcd954901a6572fa184f
Yuchen Fan <yuchen.fan@ibm.com>
2021-04-13 11:33:56 -0400

    retry for failed cert resource deploying

--- controllers/certmanager/certmanager.go
+++ controllers/certmanager/certmanager.go
@@ -44,13 +44,20 @@ func DeployCR(bs *bootstrap.Bootstrap) {
 	}
 
 	for _, cr := range DeployCRs {
-		deployResource(bs, cr)
+		for {
+			done := deployResource(bs, cr)
+			if done {
+				break
+			}
+			time.Sleep(30 * time.Second)
+		}
+
 	}
 }
 
 func waitResourceReady(bs *bootstrap.Bootstrap, apiGroupVersion string, kind string) error {
 	dc := discovery.NewDiscoveryClientForConfigOrDie(bs.Config)
-	if err := utilwait.PollImmediateInfinite(time.Second*10, func() (done bool, err error) {
+	if err := utilwait.PollImmediateInfinite(time.Second*30, func() (done bool, err error) {
 		exist, err := bs.ResourceExists(dc, apiGroupVersion, kind)
 		if err != nil {
 			return exist, err
@@ -65,7 +72,7 @@ func waitResourceReady(bs *bootstrap.Bootstrap, apiGroupVersion string, kind str
 	return nil
 }
 
-func deployResource(bs *bootstrap.Bootstrap, cr string) {
+func deployResource(bs *bootstrap.Bootstrap, cr string) bool {
 	if err := utilwait.PollImmediateInfinite(time.Second*10, func() (done bool, err error) {
 		err = bs.CreateOrUpdateFromYaml([]byte(util.Namespacelize(cr, placeholder, bs.MasterNamespace)))
 		if err != nil {
@@ -73,6 +80,8 @@ func deployResource(bs *bootstrap.Bootstrap, cr string) {
 		}
 		return true, nil
 	}); err != nil {
-		klog.Errorf("Failed to create Certmanager resource: %v", err)
+		klog.Errorf("Failed to create Certmanager resource: %v, retry in 30 seconds", err)
+		return false
 	}
+	return true
 }
