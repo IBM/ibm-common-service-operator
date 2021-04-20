0c1638adc19614c2cfd39231484491df75fcd905
Yuchen Fan <yuchen.fan@ibm.com>
2021-04-20 17:43:01 -0400

    remove logging for 3.6.4

--- controllers/storageclass/storageclass.go
+++ controllers/storageclass/storageclass.go
@@ -30,11 +30,4 @@ const Template = `
   spec:
     prometheusExt:
       storageClassName: placeholder
-- name: ibm-elastic-stack-operator
-  spec:
-    elasticStack:
-      elasticsearch:
-        data:
-          storage:
-            storageClass: placeholder
 `
