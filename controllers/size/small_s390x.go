572e7212dc741592b303537a14ee28e0fea4ec27
Yuchen Fan <yuchen.fan@ibm.com>
2021-08-15 12:27:21 -0400

    update MongoDB metrics size

--- controllers/size/small_s390x.go
+++ controllers/size/small_s390x.go
@@ -63,6 +63,14 @@ const Small = `
         requests:
           cpu: 500m
           memory: 700Mi
+      metrics:
+        resources:
+          requests:
+            cpu: 100m
+            memory: 300Mi
+          limits:
+            cpu: 1000m
+            memory: 350Mi
 - name: ibm-iam-operator
   spec:
     authentication:
