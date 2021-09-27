eba18f8a6549924a751db4fe9632227343c96bd8
Artur Obrzut <54812713+arturobrzut@users.noreply.github.com>
2021-09-27 17:17:18 +0200

    crossplane configuration from env var (#644)
    change mechanism of reading cross-plane configuration

--- controllers/constant/crossplane.go
+++ controllers/constant/crossplane.go
@@ -39,7 +39,7 @@ metadata:
     ibm-crossplane-provider: {{ .CrossplaneProvider }}
 spec:
   ignoreCrossplaneConstraints: false
-  package: 'quay.io/opencloudio/ibm-crossplane-bedrock-shim-config:1.0.0'
+  package: FromEnvVar
   packagePullPolicy: Always
   revisionActivationPolicy: Automatic
   revisionHistoryLimit: 1
