cceab699d290d43db508e90f6c8340c3a3c36d21
Jiaming Hu <Jiaming.Hu@ibm.com>
2021-10-12 10:24:10 -0400

    onboard bts

--- controllers/constant/odlm.go
+++ controllers/constant/odlm.go
@@ -267,6 +267,14 @@ spec:
     packageName: ibm-apicatalog
     scope: public
     installPlanApproval: {{ .ApprovalMode }}
+  - channel: {{ .Channel }}
+    name: ibm-bts-operator
+    namespace: {{ .MasterNs }}
+    packageName: ibm-bts-operator
+    scope: public
+    installPlanApproval: {{ .ApprovalMode }}
+    sourceName: {{ .CatalogSourceName }}
+    sourceNamespace: {{ .CatalogSourceNs }}
 `
 
 const CSV3SaasOperandConfig = `
@@ -421,6 +429,14 @@ spec:
     name: ibm-zen-operator
     namespace: {{ .MasterNs }}
     packageName: ibm-zen-operator
+    scope: public
+    installPlanApproval: {{ .ApprovalMode }}
+    sourceName: {{ .CatalogSourceName }}
+    sourceNamespace: {{ .CatalogSourceNs }}
+  - channel: {{ .Channel }}
+    name: ibm-bts-operator
+    namespace: {{ .MasterNs }}
+    packageName: ibm-bts-operator
     scope: public
     installPlanApproval: {{ .ApprovalMode }}
     sourceName: {{ .CatalogSourceName }}
