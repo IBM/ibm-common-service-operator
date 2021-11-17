649c2a06e56f365153a6d257823f0befa6519a83
Jiaming Hu <horis233@gmail.com>
2021-11-17 15:35:43 -0500

    onboard redis service (#705)

--- controllers/constant/odlm.go
+++ controllers/constant/odlm.go
@@ -401,6 +401,12 @@ spec:
     packageName: ibm-bts-operator
     scope: public
     installPlanApproval: {{ .ApprovalMode }}
+  - channel: v1.3
+    name: ibm-cloud-databases-redis-operator
+    namespace: {{ .MasterNs }}
+    packageName: ibm-cloud-databases-redis-operator
+    scope: public
+    installPlanApproval: {{ .ApprovalMode }}
 `
 
 const CSV3SaasOperandConfig = `
