e9ee4a9df77fb827910682a5e224069d0b5bb9eb
Jiaming Hu <Jiaming.Hu@ibm.com>
2022-02-14 11:24:05 -0500

    update message

--- controllers/bootstrap/init.go
+++ controllers/bootstrap/init.go
@@ -660,7 +660,7 @@ func (b *Bootstrap) CreateOrUpdateFromYaml(yamlContent []byte, alwaysUpdate ...b
 		}
 
 		if obj.GetDeletionTimestamp() != nil {
-			errMsg = fmt.Errorf("resource %s/%s is being deleted , kind: %s, apiversion: %s/%s", obj.GetNamespace(), obj.GetName(), gvk.Kind, gvk.Group, gvk.Version)
+			errMsg = fmt.Errorf("resource %s/%s is being deleted, retry later, kind: %s, apiversion: %s/%s", obj.GetNamespace(), obj.GetName(), gvk.Kind, gvk.Group, gvk.Version)
 			continue
 		}
 
