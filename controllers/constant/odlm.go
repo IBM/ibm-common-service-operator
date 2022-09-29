451cbe3f533392a0ffc65ea768576701270b361c
Ben Luzarraga <31223504+bluzarraga@users.noreply.github.com>
2022-09-29 12:51:58 -0500

    temp hard code zen cpp @ v3.21 chan for cicd builds

--- controllers/constant/odlm.go
+++ controllers/constant/odlm.go
@@ -427,7 +427,7 @@ spec:
     packageName: ibm-user-data-services-operator
     scope: public
     installPlanApproval: {{ .ApprovalMode }}
-  - channel: {{ .Channel }}
+  - channel: v3.21
     name: ibm-zen-cpp-operator
     namespace: {{ .MasterNs }}
     packageName: zen-cpp-operator
