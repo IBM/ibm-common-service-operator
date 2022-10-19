8d5012be618276e2e045a6a9f48338bb26cf7104
Allen Li <46284272+qpdpQ@users.noreply.github.com>
2022-10-19 17:20:09 -0400

    fipsEnable (#877)
    * add fipsEnable to CS cr
    
    Signed-off-by: qpdpQ <liyuchen223@gmail.com>
    
    * deprecate fipsEnabled in individual services
    
    Signed-off-by: qpdpQ <liyuchen223@gmail.com>
    
    * add boolean comparsion
    
    Signed-off-by: qpdpQ <liyuchen223@gmail.com>
    
    * change default value to false
    
    Signed-off-by: qpdpQ <liyuchen223@gmail.com>
    
    * update log info
    
    Signed-off-by: qpdpQ <liyuchen223@gmail.com>
    
    * change label for nginxIngress
    
    Signed-off-by: qpdpQ <liyuchen223@gmail.com>
    
    Signed-off-by: qpdpQ <liyuchen223@gmail.com>

--- controllers/operandconfig.go
+++ controllers/operandconfig.go
@@ -184,10 +184,12 @@ func mergeChangedMap(key string, defaultMap interface{}, changedMap interface{},
 				finalMap[key] = defaultMap
 			} else {
 				var comparableKeys = map[string]bool{
-					"replicas": true,
-					"cpu":      true,
-					"memory":   true,
-					"profile":  true,
+					"replicas":     true,
+					"cpu":          true,
+					"memory":       true,
+					"profile":      true,
+					"fipsEnabled":  true,
+					"fips_enabled": true,
 				}
 				if _, ok := comparableKeys[key]; ok {
 					if directAssign {
