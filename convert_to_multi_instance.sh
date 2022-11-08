4ddce2972ee266bee95f78cfec6a61c2f77e2778
Ben Luzarraga <31223504+bluzarraga@users.noreply.github.com>
2022-11-08 15:15:23 -0600

    handle nss deletion error when nss not present (#917)
    * handle nss deletion error when nss not present
    
    Signed-off-by: Ben Luzarraga <luzarragaben@ibm.com>
    
    * typo
    
    Signed-off-by: Ben Luzarraga <luzarragaben@ibm.com>
    
    Signed-off-by: Ben Luzarraga <luzarragaben@ibm.com>
    Co-authored-by: Ben Luzarraga <luzarragaben@ibm.com>

--- convert_to_multi_instance.sh
+++ convert_to_multi_instance.sh
@@ -342,16 +342,26 @@ function check_cm_ns_exist(){
 }
 
 function removeNSS(){
-    ${OC} get nss --all-namespaces | grep nss-managedby-odlm | while read -r line; do
-        local namespace=$(echo $line | awk '{print $1}')
-        info "deleting namespace scope nss-managedby-odlm in namespace $namespace"
-        ${OC} delete nss nss-managedby-odlm -n ${namespace} || error "unable to delete namespace scope nss-managedby-odlm in ${namespace}"
-    done
-    ${OC} get nss --all-namespaces | grep odlm-scope-managedby-odlm | while read -r line; do
-        local namespace=$(echo $line | awk '{print $1}')
-        info "deleting namespace scope odlm-scope-managedby-odlm in namespace $namespace"
-        ${OC} delete nss odlm-scope-managedby-odlm -n ${namespace} || error "unable to delete namespace scope odlm-scope-managedby-odlm in ${namespace}"
-    done
+    failcheck=$(${OC} get nss --all-namespaces | grep nss-managedby-odlm || echo "failed")
+    if [[ $failcheck != "failed" ]]; then
+        ${OC} get nss --all-namespaces | grep nss-managedby-odlm | while read -r line; do
+            local namespace=$(echo $line | awk '{print $1}')
+            info "deleting namespace scope nss-managedby-odlm in namespace $namespace"
+            ${OC} delete nss nss-managedby-odlm -n ${namespace} || error "unable to delete namespace scope nss-managedby-odlm in ${namespace}"
+        done
+    else
+        info "Namespace Scope CR \"nss-managedby-odlm\" not present. Moving on..."
+    fi
+    failcheck=$(${OC} get nss --all-namespaces | grep odlm-scope-managedby-odlm || echo "failed")
+    if [[ $failcheck != "failed" ]]; then
+        ${OC} get nss --all-namespaces | grep odlm-scope-managedby-odlm | while read -r line; do
+            local namespace=$(echo $line | awk '{print $1}')
+            info "deleting namespace scope odlm-scope-managedby-odlm in namespace $namespace"
+            ${OC} delete nss odlm-scope-managedby-odlm -n ${namespace} || error "unable to delete namespace scope odlm-scope-managedby-odlm in ${namespace}"
+        done
+    else
+        info "Namespace Scope CR \"odlm-scope-managedby-odlm\" not present. Moving on..."
+    fi
 }
 
 function msg() {
