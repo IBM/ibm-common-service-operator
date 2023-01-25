85c98c706df75685cc13014bfc4caa892bbfd58b
Ben Luzarraga <31223504+bluzarraga@users.noreply.github.com>
2023-01-25 14:41:38 -0600

    rm rh marketplace operator from operand registry (#991)
    * rm rh marketplace operator from operand registry
    
    * remove rh marketplace operator from operand config

--- controllers/constant/odlm.go
+++ controllers/constant/odlm.go
@@ -460,18 +460,6 @@ spec:
         force: true
         kind: Job
         name: pre-zen-operand-config-job 
-  - name: redhat-marketplace-operator
-    resources:
-      - apiVersion: ibmcpcs.ibm.com/v1
-        kind: SecretShare
-        name: redhat-marketplace-operator
-        namespace: {{ .CPFSNs }}
-        data:
-          spec:
-            configmapshares:
-              - configmapname: namespace-scope
-                sharewith:
-                  - namespace: openshift-redhat-marketplace
 `
 
 const CSV3OperandRegistry = `
@@ -629,14 +617,6 @@ spec:
     scope: public
     installPlanApproval: {{ .ApprovalMode }}
     sourceName: {{ .CatalogSourceName }}
-    sourceNamespace: {{ .CatalogSourceNs }}
-  - channel: stable
-    name: redhat-marketplace-operator
-    namespace: openshift-redhat-marketplace
-    packageName: redhat-marketplace-operator
-    scope: public
-    installPlanApproval: {{ .ApprovalMode }}
-    sourceName: certified-operators
     sourceNamespace: {{ .CatalogSourceNs }}
   - channel: {{ .Channel }}
     name: ibm-zen-operator
@@ -1017,18 +997,6 @@ spec:
         force: true
         kind: Job
         name: pre-zen-operand-config-job 
-  - name: redhat-marketplace-operator
-    resources:
-      - apiVersion: ibmcpcs.ibm.com/v1
-        kind: SecretShare
-        name: redhat-marketplace-operator
-        namespace: {{ .CPFSNs }}
-        data:
-          spec:
-            configmapshares:
-              - configmapname: namespace-scope
-                sharewith:
-                  - namespace: openshift-redhat-marketplace
 `
 
 const CSV3SaasOperandRegistry = `
