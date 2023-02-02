53b554d3cd1ca7a4c6487eae6f914a27893ae615
Henry Li <henry.h.li@ibm.com>
2023-02-02 13:23:45 -0500

    fixed cert-manager test linter error by removing re-declaration of cfg
    Signed-off-by: Henry Li <henry.h.li@ibm.com>

--- controllers/cert-manager/suite_test.go
+++ controllers/cert-manager/suite_test.go
@@ -37,7 +37,7 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config //nolint:deadcode,unused
+var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
 
@@ -58,7 +58,8 @@ var _ = BeforeSuite(func() {
 		ErrorIfCRDPathMissing: true,
 	}
 
-	cfg, err := testEnv.Start()
+	var err error
+	cfg, err = testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
