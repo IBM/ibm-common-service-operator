d39894090d14de8f24fa6c0ab1a406948dd673e1
Daniel Fan <fanyuchensx@gmail.com>
2023-02-27 16:47:49 -0500

    Update cp3pt0 deployment folder (#1046)
    * Update cp3pt0 deployment folder
    
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>
    
    * update script permission
    
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>

---
+++ cp3pt0-deployment/common/utils.sh
@@ -0,0 +1,323 @@
+#!/bin/bash
+
+# Licensed Materials - Property of IBM
+# Copyright IBM Corporation 2023. All Rights Reserved
+# US Government Users Restricted Rights -
+# Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+# This is an internal component, bundled with an official IBM product. 
+# Please refer to that particular license for additional information. 
+
+# ---------- Info functions ----------
+
+function msg() {
+    printf '%b\n' "$1"
+}
+
+function info() {
+    msg "[INFO] ${1}"
+}
+
+function success() {
+    msg "\33[32m[✔] ${1}\33[0m"
+}
+
+function warning() {
+    msg "\33[33m[✗] ${1}\33[0m"
+}
+
+function error() {
+    msg "\33[31m[✘] ${1}\33[0m"
+    exit 1
+}
+
+function title() {
+    msg "\33[34m# ${1}\33[0m"
+}
+
+function debug() {
+    msg "\33[33m[DEBUG] ${1}\33[0m"
+}
+
+
+# ---------- Check functions ----------
+
+function check_command() {
+    local command=$1
+
+    if [[ -z "$(command -v ${command} 2> /dev/null)" ]]; then
+        error "${command} command not available"
+    else
+        success "${command} command available"
+    fi
+}
+
+function check_return_code() {
+    local rc=$1
+    local error_message=$2
+    
+    if [ "${rc}" -ne 0 ]; then
+        error "${error_message}"
+    else
+        return 0
+    fi
+}
+
+function restart_job() {
+    local namespace=$1
+    local job_name=$2
+
+    if [[ ! -z "$(${OC} -n ${namespace} get job ${job_name} --ignore-not-found)" ]]; then
+        ${OC} -n ${namespace} patch job ${job_name} --type json -p \
+            '[{ "op": "remove", "path": "/spec/selector"}, 
+              { "op": "remove", "path": "/spec/template/metadata/labels/controller-uid"}]' \
+            -o yaml --dry-run \
+            | ${OC} -n ${namespace} replace --force --timeout=20s -f - 2> /dev/null
+    else
+        error "Job not found: ${job_name}"
+    fi
+}
+
+function translate_step() {
+    local step=$1
+    echo "${step}" | tr '[1-9]' '[a-i]'
+}
+
+function wait_for_condition() {
+    local condition=$1
+    local retries=$2
+    local sleep_time=$3
+    local wait_message=$4
+    local success_message=$5
+    local error_message=$6
+
+    info "${wait_message}"
+    while true; do
+        result=$(eval "${condition}")
+
+        if [[ ( ${retries} -eq 0 ) && ( -z "${result}" ) ]]; then
+            error "${error_message}"
+        fi
+ 
+        sleep ${sleep_time}
+        result=$(eval "${condition}")
+        
+        if [[ -z "${result}" ]]; then
+            info "RETRYING: ${wait_message} (${retries} left)"
+            retries=$(( retries - 1 ))
+        else
+            break
+        fi
+    done
+
+    if [[ ! -z "${success_message}" ]]; then
+        success "${success_message}"
+    fi
+}
+
+
+function wait_for_not_condition() {
+    local condition=$1
+    local retries=$2
+    local sleep_time=$3
+    local wait_message=$4
+    local success_message=$5
+    local error_message=$6
+
+    info "${wait_message}"
+    while true; do
+        result=$(eval "${condition}")
+
+        if [[ ( ${retries} -eq 0 ) && ( ! -z "${result}" ) ]]; then
+            error "${error_message}"
+        fi
+ 
+        sleep ${sleep_time}
+        result=$(eval "${condition}")
+        
+        if [[ ! -z "${result}" ]]; then
+            info "RETRYING: ${wait_message} (${retries} left)"
+            retries=$(( retries - 1 ))
+        else
+            break
+        fi
+    done
+
+    if [[ ! -z "${success_message}" ]]; then
+        success "${success_message}"
+    fi
+}
+
+function wait_for_configmap() {
+    local namespace=$1
+    local name=$2
+    local condition="${OC} -n ${namespace} get cm --no-headers --ignore-not-found | grep ^${name}"
+    local retries=12
+    local sleep_time=10
+    local total_time_mins=$(( sleep_time * retries / 60))
+    local wait_message="Waiting for ConfigMap ${name} in namespace ${namespace} to be made available"
+    local success_message="ConfigMap ${name} in namespace ${namespace} is available"
+    local error_message="Timeout after ${total_time_mins} minutes waiting for ConfigMap ${name} in namespace ${namespace} to become available"
+ 
+    wait_for_condition "${condition}" ${retries} ${sleep_time} "${wait_message}" "${success_message}" "${error_message}"
+}
+
+function wait_for_pod() {
+    local namespace=$1
+    local name=$2
+    local condition="${OC} -n ${namespace} get po --no-headers --ignore-not-found | egrep 'Running|Completed|Succeeded' | grep ^${name}"
+    local retries=30
+    local sleep_time=30
+    local total_time_mins=$(( sleep_time * retries / 60))
+    local wait_message="Waiting for pod ${name} in namespace ${namespace} to be running"
+    local success_message="Pod ${name} in namespace ${namespace} is running"
+    local error_message="Timeout after ${total_time_mins} minutes waiting for pod ${name} in namespace ${namespace} to be running"
+ 
+    wait_for_condition "${condition}" ${retries} ${sleep_time} "${wait_message}" "${success_message}" "${error_message}"
+}
+
+function wait_for_project() {
+    local name=$1
+    local condition="${OC} get project ${name} --no-headers --ignore-not-found"
+    local retries=50
+    local sleep_time=10
+    local total_time_mins=$(( sleep_time * retries / 60))
+    local wait_message="Waiting for project ${name} to be created"
+    local success_message="Project ${name} is created"
+    local error_message="Timeout after ${total_time_mins} minutes waiting for project ${name} to be created"
+ 
+    wait_for_condition "${condition}" ${retries} ${sleep_time} "${wait_message}" "${success_message}" "${error_message}"
+}
+
+function wait_for_operator() {
+    local namespace=$1
+    local operator_name=$2
+    local condition="${OC} -n ${namespace} get csv --no-headers --ignore-not-found | egrep 'Succeeded' | grep ^${operator_name}"
+    local retries=50
+    local sleep_time=10
+    local total_time_mins=$(( sleep_time * retries / 60))
+    local wait_message="Waiting for operator ${operator_name} in namespace ${namespace} to be made available"
+    local success_message="Operator ${operator_name} in namespace ${namespace} is available"
+    local error_message="Timeout after ${total_time_mins} minutes waiting for ${operator_name} in namespace ${namespace} to become available"
+ 
+    wait_for_condition "${condition}" ${retries} ${sleep_time} "${wait_message}" "${success_message}" "${error_message}"
+}
+
+function wait_for_service_account() {
+    local namespace=$1
+    local name=$2
+    local condition="${OC} -n ${namespace} get sa ${name} --no-headers --ignore-not-found"
+    local retries=20
+    local sleep_time=10
+    local total_time_mins=$(( sleep_time * retries / 60))
+    local wait_message="Waiting for service account ${name} to be created"
+    local success_message="Service account ${name} is created"
+    local error_message="Timeout after ${total_time_mins} minutes waiting for service account ${name} to be created"
+ 
+    wait_for_condition "${condition}" ${retries} ${sleep_time} "${wait_message}" "${success_message}" "${error_message}"
+}
+
+function wait_for_operand_request() {
+    local namespace=$1
+    local name=$2
+    local condition="${OC} -n ${namespace} get operandrequests ${name} --no-headers --ignore-not-found -o jsonpath='{.status.phase}' | grep 'Running'"
+    local retries=20
+    local sleep_time=10
+    local total_time_mins=$(( sleep_time * retries / 60))
+    local wait_message="Waiting for operand request ${name} to be running"
+    local success_message="Operand request ${name} is running"
+    local error_message="Timeout after ${total_time_mins} minutes waiting for operand request ${name} to be running"
+ 
+    wait_for_condition "${condition}" ${retries} ${sleep_time} "${wait_message}" "${success_message}" "${error_message}"
+}
+
+function is_sub_exist() {
+    local package_name=$1
+    if [ $# -eq 2 ]; then
+        local namespace=$2
+        local name=$(${OC} get sub -n ${namespace} -o yaml -o jsonpath='{.items[*].spec.name}')
+    else
+        local name=$(${OC} get sub -A -o yaml -o jsonpath='{.items[*].spec.name}')
+    fi
+    is_exist=$(echo "$name" | grep -w "$package_name")
+}
+
+# ---------- creation functions ----------
+
+function create_namespace() {
+    local namespace=$1
+
+    title "Creating namespace ${namespace}\n"
+    
+    if [[ -z "$(${OC} get namespace ${namespace} --ignore-not-found)" ]]; then
+        ${OC} create namespace ${namespace}
+        if [[ $? -ne 0 ]]; then
+            error "Error creating namespace ${namespace}"
+        fi
+    else
+        info "Namespace ${namespace} already exists. Skip creating"
+    fi
+}
+
+function create_operator_group() {
+    local name=$1
+    local ns=$2
+    local target=$3
+    local og=$(
+        cat <<EOF
+apiVersion: operators.coreos.com/v1
+kind: OperatorGroup
+metadata:
+  name: $name
+  namespace: $ns
+spec: $target
+EOF
+    )
+
+    info "Checking existing OperatorGroup in $ns:\n"
+    existing_og=$(${OC} get operatorgroup -n $ns --no-headers --ignore-not-found | wc -l)
+    if [[ ${existing_og} -ne 0 ]]; then
+        info "OperatorGroup already exists in $ns. Skip creating"
+        return 0
+    fi
+    echo
+    info "Creating following OperatorGroup:\n"
+    echo "$og"
+    echo "$og" | ${OC} apply -f -
+    if [[ $? -ne 0 ]]; then
+        error "Failed to create OperatorGroup ${name} in ${ns}"
+    fi
+}
+
+function create_subscription() {
+    local name=$1
+    local ns=$2
+    local channel=$3
+    local package_name=$4
+    local source=$5
+    local source_ns=$6
+    local install_mode=$7
+    local sub=$(
+        cat <<EOF
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: $name
+  namespace: $ns
+spec:
+  channel: $channel
+  installPlanApproval: $install_mode
+  name: $package_name
+  source: $source
+  sourceNamespace: $source_ns
+EOF
+    )
+    
+    echo
+    info "Creating following Subscription:\n"
+    echo "$sub"
+    echo "$sub" | ${OC} apply -f -
+    if [[ $? -ne 0 ]]; then
+        error "Failed to create subscription ${name} in ${ns}"
+    fi
+}
---
+++ cp3pt0-deployment/setup_singleton.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+# Licensed Materials - Property of IBM
+# Copyright IBM Corporation 2023. All Rights Reserved
+# US Government Users Restricted Rights -
+# Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+# This is an internal component, bundled with an official IBM product. 
+# Please refer to that particular license for additional information. 
+
+# ---------- Command arguments ----------
+OC=oc
+ENABLE_LICENSING=0
+CHANNEL="v4.0"
+SOURCE="opencloud-operators"
+SOURCE_NS="openshift-marketplace"
+INSTALL_MODE="Automatic"
+
+# ---------- Command variables ----------
+
+# script base directory
+BASE_DIR=$(cd $(dirname "$0")/$(dirname "$(readlink $0)") && pwd -P)
+
+# counter to keep track of installation steps
+STEP=0
+
+# ---------- Main functions ----------
+
+. ${BASE_DIR}/common/utils.sh
+
+function main() {
+    parse_arguments "$@"
+    pre_req
+    install_cert_manager
+    install_licensing
+}
+
+function parse_arguments() {
+    # process options
+    while [[ "$@" != "" ]]; do
+        case "$1" in
+        --oc)
+            shift
+            OC=$1
+            ;;
+        --enable-licensing)
+            ENABLE_LICENSING=1
+            ;;
+        -c | --channel)
+            shift
+            CHANNEL=$1
+            ;;
+        -s | --source)
+            shift
+            SOURCE=$1
+            ;;
+        -n | --source-namespace)
+            shift
+            SOURCE_NS=$1
+            ;;
+        -h | --help)
+            print_usage
+            exit 1
+            ;;
+        *) 
+            echo "wildcard"
+            ;;
+        esac
+        shift
+    done
+}
+
+function print_usage() {
+    script_name=`basename ${0}`
+    echo "Usage: ${script_name} [OPTIONS]..."
+    echo ""
+    echo "Install Cloud Pak 3 pre-reqs if they do not already exist: ibm-cert-manager-operator and optionally ibm-licensing-operator"
+    echo "The ibm-cert-manager-operator will be installed in namespace ibm-cert-manager"
+    echo "The ibm-licensing-operator will be installed in namespace ibm-licensing"
+    echo ""
+    echo "Options:"
+    echo "   --oc string                    File path to oc CLI. Default uses oc in your PATH"
+    echo "   --enable-licensing             Set this flag to install ibm-licensing-operator"
+    echo "   -c, --channel string           Channel for Subscription(s). Default is v4.0"   
+    echo "   -i, --install-mode string      InstallPlan Approval Mode. Default is Automatic. Set to Manual for manual approval mode"
+    echo "   -s, --source string            CatalogSource name. This assumes your CatalogSource is already created. Default is opencloud-operators"
+    echo "   -n, --namespace string         Namespace of CatalogSource. Default is openshift-marketplace"
+    echo "   -h, --help                     Print usage information"
+    echo ""
+}
+
+function install_cert_manager() {
+    title "Installing cert-manager\n"
+    is_sub_exist "cert-manager" # this will catch the packagenames of all cert-manager-operators
+    if [ $? -eq 0 ]; then
+        warning "There is a cert-manager Subscription already\n"
+        return 0
+    fi
+
+    pods_exist=$(${OC} get pods -A | grep -w cert-manager-webhook)
+    if [ $? -eq 0 ]; then
+        warning "There is a cert-manager-webhook pod Running, so most likely another cert-manager is already installed\n"
+        return 0
+    fi
+
+    create_namespace ibm-cert-manager
+    create_operator_group "ibm-cert-manager-operator" "ibm-cert-manager" "{}"
+    create_subscription "ibm-cert-manager-operator" "ibm-cert-manager" "$CHANNEL" "ibm-cert-manager-operator" "${SOURCE}" "${SOURCE_NS}" "${INSTALL_MODE}"
+    wait_for_operator "ibm-cert-manager" "ibm-cert-manager-operator"
+}
+
+function install_licensing() {
+    if [ $ENABLE_LICENSING -ne 1 ]; then
+        return
+    fi
+
+    title "Installing licensing\n"
+    is_sub_exist "ibm-licensing-operator-app" # this will catch the packagenames of all cert-manager-operators
+    if [ $? -eq 0 ]; then
+        warning "There is an ibm-licensing-operator Subscription already\n"
+        return 0
+    fi
+
+    create_namespace ibm-licensing
+
+    target=$(cat <<EOF
+        
+  targetNamespaces:
+    - ibm-licensing
+EOF
+)
+    create_operator_group "ibm-licensing-operator-app" "ibm-licensing" "$target"
+    create_subscription "ibm-licensing-operator-app" "ibm-licensing" "$CHANNEL" "ibm-licensing-operator-app" "${SOURCE}" "${SOURCE_NS}" "${INSTALL_MODE}"
+    wait_for_operator "ibm-licensing" "ibm-licensing-operator"
+}
+
+function pre_req() {
+    check_command "${OC}"
+
+    # checking oc command logged in
+    user=$(oc whoami 2> /dev/null)
+    if [ $? -ne 0 ]; then
+        error "You must be logged into the OpenShift Cluster from the oc command line"
+    else
+        success "oc command logged in as ${user}"
+    fi
+}
+
+main $*
