8750a5e64b48e356cb67655ee6c74a2d8e7db1cd
Allen Li <46284272+qpdpQ@users.noreply.github.com>
2023-03-24 15:12:57 -0400

    add sleep back (#1106)
    Signed-off-by: Allen Li <liyuchen223@gmail.com>
    Co-authored-by: Daniel Fan <fanyuchensx@gmail.com>

--- cp3pt0-deployment/setup_tenant.sh
+++ cp3pt0-deployment/setup_tenant.sh
@@ -303,6 +303,7 @@ function install_cs_operator() {
         sleep 120
     fi
     wait_for_operator "$OPERATOR_NS" "ibm-common-service-operator"
+    sleep 120
     wait_for_nss_patch "$OPERATOR_NS" 
     configure_cs_kind
 }
