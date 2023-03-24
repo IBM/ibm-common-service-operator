2cbbe82c11541a3bae6e68a2162b6ad2c343875c
YuChen <yuchen.shen@mail.utoronto.ca>
2023-03-23 20:54:51 -0700

    reorganize nsMapping updating
    Signed-off-by: YuChen <yuchen.shen@mail.utoronto.ca>

--- controllers/common/util.go
+++ controllers/common/util.go
@@ -555,27 +555,27 @@ func UpdateCsMaps(cm *corev1.ConfigMap, requestNsList, servicesNS, operatorNs st
 	newnsMapping.CsNs = servicesNS
 
 	for _, nsMapping := range cmData.NsMappingList {
+		// OperatorNs already exists in common service namespace
 		if operatorNs == nsMapping.CsNs {
-			// OperatorNs already exists in common service namespace
 			alreadyExists = true
-			cmData.NsMappingList[count] = newnsMapping
 			break
 		}
 
 		for _, ns := range nsMapping.RequestNs {
+			// OperatorNs already exists in request namespaces list
 			if operatorNs == ns {
-				// OperatorNs already exists in request namespaces list
 				alreadyExists = true
-				cmData.NsMappingList[count] = newnsMapping
 				break
 			}
 		}
 		count++
 	}
 
-	// Create a new namespaceMapping
+	// Create/Update namespaceMapping entry
 	if !alreadyExists {
 		cmData.NsMappingList = append(cmData.NsMappingList, newnsMapping)
+	} else {
+		cmData.NsMappingList[count] = newnsMapping
 	}
 
 	commonServiceMap, error := utilyaml.Marshal(&cmData)
