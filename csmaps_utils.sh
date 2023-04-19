025d89951482d0ddd7b57da67accb7225e93b14a
Henry Li <henry.h.li@ibm.com>
2023-04-19 16:36:57 -0400

    updated csmaps_utils with create, read, and update operations
    Signed-off-by: Henry Li <henry.h.li@ibm.com>

--- csmaps_utils.sh
+++ csmaps_utils.sh
@@ -15,321 +15,109 @@
 # limitations under the License.
 #
 
-set -o errexit
-set -o pipefail
-set -o errtrace
 set -o nounset
 
 OC=oc
 YQ=yq
-master_ns=
-excluded_raw=
-insert_raw=
-map_to_cs_ns=
-requested_ns=
-OPERATOR_NS=""
-SERVICES_NS=""
-TETHERED_NS=""
-CONTROL_NS=""
-NEW_MAPPING=""
-cm_name="common-service-maps"
-cm_maps=
+CM_NAME="mock-cs-maps"
+CONTROL_NS="cs-control"
 
-function main () {
-    while [ "$#" -gt "0" ]
-    do
-        case "$1" in
-        "-h"|"--help")
-            usage
-            exit 0
-            ;;
-        "--original-cs-ns")
-            master_ns=$2
-            shift
-            ;;
-        "--excluded-ns")
-            excluded_raw=$2
-            shift
-            ;;
-        "--insert-ns")
-            insert_raw=$2
-            shift
-            ;;
-        "--control-ns")
-            CONTROL_NS=$2
-            shift
-            ;;
-        *)
-            error "invalid option -- \`$1\`. Use the -h or --help option for usage info."
-            ;;
-        esac
-        shift
-    done
-    which "${OC}" || error "Missing oc CLI"
-    which "${YQ}" || error "Missing yq"
+# update_cs_maps Updates the common-service-maps with the given yaml. Note that
+# the given yaml should have the right indentation/padding, minimum 2 spaces per
+# line. If there are multiple lines in the yaml, ensure that each line has
+# correct indentation.
+function update_cs_maps() {
+    local yaml=$1
 
-    if [[ -z $CONTROL_NS ]] &&  [[ -z $master_ns ]]; then
-        usage
-        error "No parameters entered. Please re-run specifying original and control namespace values. Use -h for help."
-    elif [[ -z $CONTROL_NS ]] || [[ -z $master_ns ]]; then
-        usage
-        error "Required parameters missing. Please re-run specifying original and control namespace values. Use -h for help."
-    fi
-    #need to get the namespaces for csmaps generation before pausing cs, otherwise namespace-scope cm does not include all namespaces
-    cm_maps=$(oc get -n kube-public cm ${cm_name} -o yaml | yq '.data.["common-service-maps.yaml"]')
+    local object="$(
+        cat <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: "$CM_NAME"
+  namespace: kube-public
+data:
+  common-service-maps.yaml: |
+${yaml}
+EOF
+)"
+    echo "$object" | oc apply -f -
 }
 
-function usage() {
-	local script="${0##*/}"
-
-	while read -r ; do echo "${REPLY}" ; done <<-EOF
-	Usage: ${script} [OPTION]...
-	Isolate and prepare common services for upgrade
-	Options:
-	Mandatory arguments to long options are mandatory for short options too.
-    -h, --help                    display this help and exit
-    --original-cs-ns              specify the namespace the original common services installation resides in
-    --control-ns                  specify the control namespace value in the common-service-maps configmap
-    --excluded-ns                 specify namespaces to be excluded from the common-service-maps configmap. Comma separated no spaces.
-    --insert-ns                 specify namespaces to be inserted into the common-service-maps configmap. Comma separated no spaces.
-	EOF
-}
-
+# create_empty_csmaps Creates a new common-service-maps configmap and inserts
+# an empty common-service-maps.yaml field.
+#
+# If the common-service-maps already exists, then will error
 function create_empty_csmaps() {
     title " Creating empty common-service-maps configmap "
-    #what does an empty cs maps look like? Is the datat fiel empty, the embedded yaml empty, or the requested from/map to empty?
-    local object=$(
-            cat <<EOF
-apiVersion: v1
-kind: ConfigMap
-metadata:
-    name: common-service-maps
-    namespace: kube-public
-data:
-    common-service-maps.yaml: |
-$(echo "$NEW_MAPPING" | awk '{print "        "$0}')
-EOF
-)
-
-        echo "$object" | ${OC} apply -f -
-
+    local isExists=$("${OC}" get configmap --ignore-not-found -n kube-public "$CM_NAME")
+    if [ ! -z "$isExists" ]; then
+        info "The $CM_NAME already exists, skipping"
+        return
+    fi
+    update_cs_maps ""
     success "Empty common-service-maps configmap created in kube-public namespace"
-
 }
 
+# insert_control_ns Insert the controlNamespace field into the configmap if it
+# does not exist
 function insert_control_ns() {
-    return_value=$("${OC}" get configmap -n kube-public -o yaml ${cm_name} | yq '.data' | grep controlNamespace: > /dev/null || echo failed)
-    if [[ $return_value == "failed" ]]; then
-        #insert control namespace
-        ${OC} get configmap -n kube-public -o yaml ${cm_name} > tmp.csmaps.yaml
-        yq -i '.data.["common-service-maps.yaml"].controlNamespace = "'${CONTROL_NS}'"' tmp.yaml #only edits existing does not add new field
+    local current_yaml=$("${OC}" get -n kube-public cm "$CM_NAME" -o yaml | "${YQ}" '.data.["common-service-maps.yaml"]')
+
+    current=$(echo "$current_yaml" | "${YQ}" .'controlNamespace')
+    if [[ "$current" != "$CONTROL_NS" && "$current" != "" && "$current" != "null" ]]; then
+        error "The controlNamespace field in common-service-maps is already set to: $current, and cannot be changed"
     fi
-    return_value="reset"
+
+    local updated_yaml=$(echo "$current_yaml" | "${YQ}" '.controlNamespace = "'$CONTROL_NS'"')
+    local padded_yaml=$(echo "$updated_yaml" | awk '$0="    "$0')
+    update_cs_maps "$padded_yaml"
 }
 
-#the same logic as "gather_csmaps_ns" from isolate script
-function compute_ns_list () {
-    #read namespaces from ns scope cm as well as exclude/include parameters
-    return_value=$(${OC} get -n ${master_ns} cm namespace-scope > /dev/null || echo failed)
-    if [[ $return_value == "failed" ]]; then
-        error "No namespace-scope configmap in Original CS Namespace ${master_ns}. Verify namespace is correct and IBM common services is installed there."
-    else
-        namespaces=$(oc get cm namespace-scope -n "${master_ns}" -o json | jq '.data.namespaces')
-        #output namespace-scope cm
-        echo $(oc get cm namespace-scope -n "${master_ns}" -o yaml)
-        namespaces=$(echo $namespaces | tr -d '"')
-        IFS=',' read -a nsFromCM <<< "$namespaces"
-    fi
-    #remove excluded from namespaces
-    if [[ $excluded_raw != "" ]]; then
-        IFS=',' read -a excluded_ns <<< "$excluded_raw"
-        #this is very ugly but very consistent and these lists should not be too long anyway
-        for ns in ${nsFromCM[@]}
-        do
-            skip=0
-            for exns in ${excluded_ns[@]}
-            do
-                if [[ $ns == $exns ]]; then
-                    skip=1
-                    break
-                fi
-            done
-            if [[ $ns == $master_ns ]]; then
-                skip=1
-            fi
-            if [[ $skip != 1 ]]; then
-                if [[ $TETHERED_NS == "" ]]; then
-                    TETHERED_NS="$ns"
-                else
-                    TETHERED_NS="$TETHERED_NS $ns"
-                fi
-            fi
-        done
-    else
-        echo "excluded empty"
-        echo "ns from cm $nsFromCM"
-        for ns in ${nsFromCM[@]}
-        do
-            if [[ $ns != $master_ns ]]; then
-                if [[ $TETHERED_NS == "" ]]; then
-                    TETHERED_NS="$ns"
-                else
-                    TETHERED_NS="$TETHERED_NS $ns"
-                fi
-            fi
-        done
-    fi
-    if [[ $insert_raw != "" ]]; then
-        IFS=',' read -a insertNS <<< "$insert_raw"
-        for ns in ${insertNS[@]}
-        do
-            if [[ $TETHERED_NS == "" ]]; then
-                TETHERED_NS="$ns"
-            else
-                TETHERED_NS="$TETHERED_NS $ns"
-            fi
-        done
-    fi
-    if [[ $TETHERED_NS == "" ]]; then
-        TETHERED_NS=$master_ns
-    fi
-
-    OPERATOR_NS=$master_ns
-    SERVICES_NS=$master_ns
-    info "common-service-maps namespaces: $TETHERED_NS"
-}
-
+# read_tenant_from_csmaps Gets the list in requested-from-namespace for a given
+# map_to_cs_ns and prints it out. If map_to_cs_ns does not exist, then output is
+# empty
 function read_tenant_from_csmaps() {
-    local map_to_ns=$1
-    local tenant_ns_list=$(echo "$cm_maps" | yq eval '.namespaceMapping[] | select(.map-to-common-service-namespace == "'${map_to_ns}'").requested-from-namespace' | awk '{ print $2 }')
-    echo $tenant_ns_list
+    local map_to_cs_ns=$1
+    local current_yaml=$("${OC}" get -n kube-public cm "$CM_NAME" -o yaml | "${YQ}" '.data.["common-service-maps.yaml"]')
+    local tenant_ns_list=$(echo "$current_yaml" | "${YQ}" eval '.namespaceMapping[] | select(.map-to-common-service-namespace == "'${map_to_cs_ns}'").requested-from-namespace' | awk '{ print $2 }')
+    echo "$tenant_ns_list"
 }
 
+# update_tenant Updates an entire tenant in common-service-maps. The tenant is
+# identified by map_to_cs_ns, and will be updated with the given list of
+# namespaces which must be space delimited.
+#
+# If tenant does not exist, then it will be added.
+# The map_to_cs_ns will always be added to the requested-from-namespace list.
+# Before the common-service-maps is updated, the requested-from-namespace list
+# will be made unique, so that there are no duplicates
 function update_tenant() {
-    map_to_cs_ns=$1
-    namespaces=$1
-    requested_ns_delimited=""
-    for ns in $namespaces
-    do
-        if [[ $requested_ns_delimited=="" ]]; then
-            requested_ns_delimited="\"$ns\""
-        else
-            requested_ns_delimited="$requested_ns_delimited,\"$ns\""
-        fi
-    done
-    echo "$cm_maps" | yq eval 'with(.namespaceMapping[]; select(.map-to-common-service-namespace == "'${map_to_ns}'").requested-from-namespace = ["'${requested_ns_delimited}'"])'
-}
+    local map_to_cs_ns=$1
+    shift
+    local namespaces=$@
 
-function compare_ns_lists() {
-    #compare sorted compute_ns_list and sorted list from common-service-maps
-    #if different
-        #update tenant entry in common-service-maps
-}
+    local current_yaml=$("${OC}" get -n kube-public cm "$CM_NAME" -o yaml | "${YQ}" '.data.["common-service-maps.yaml"]')
+    local updated_yaml="$current_yaml"
 
-#the same logic as "contstruct_mapping" from isolate script
-function construct_mapping() {
-    NEW_MAPPING='- requested-from-namespace:'
-
-    local unique_ns_list=$(echo $OPERATOR_NS $SERVICES_NS $TETHERED_NS | tr ' ' '\n' | sort | uniq | tr '\n' ' ')
-
-    # Append tenant namespaces to NEW_MAPPING to requested-from-namespace list
-    for ns in $unique_ns_list; do
-        NEW_MAPPING="$NEW_MAPPING\n  - $ns"
-    done
-
-    # Append servicesNamespace to map-to-common-service-namespace
-    NEW_MAPPING="$NEW_MAPPING\n  map-to-common-service-namespace: $SERVICES_NS"
-}
-
-#the same logic as "mapping_topology" from isolate script
-function mapping_topology() {
-    construct_mapping
-
-    # Check if ConfigMap exists in the cluster
-    if ${OC} get configmap common-service-maps -n kube-public > /dev/null 2>&1; then
-        # ConfigMap exists, retrieve its current data
-        local current_mapping=$(${OC} get configmap common-service-maps -n kube-public -o jsonpath='{.data.common-service-maps\.yaml}')
-
-        # Remove the defaultCsNs key-value mapping if it exists
-        current_mapping=$(echo "$current_mapping" | awk '/defaultCsNs:/ {next} {print}')
-
-        # Check if servicesNamespace already exists in the map-to-common-service-namespace
-        # extract the mapped namespaces from the ConfigMap
-        map_to_ns=$(echo "$current_mapping" | yq -r '.namespaceMapping[].map-to-common-service-namespace')
-        echo "map_to_ns"
-        echo "$map_to_ns"
-        if grep -Fxq $SERVICES_NS <<< "$map_to_ns"; then
-            info "map-to-common-service-namespace $SERVICES_NS already exists in the namespaceMapping array. Skipping updating common-service-maps ConfigMap"
-            return 0
-        fi
-        
-        # Check if each tenant namespace already exists in the requested-from-namespace array
-        # extract the requested namespaces from the ConfigMap
-        requested_ns=$(echo "$current_mapping" | yq -r '.namespaceMapping[].requested-from-namespace[]')
-        echo "requested_ns"
-        echo "$requested_ns"
-        # loop over each namespace in the list and check if it exists in the ConfigMap
-        for ns in $OPERATOR_NS $SERVICES_NS $TETHERED_NS; do
-            if grep -Fxq $ns <<< "$requested_ns"; then
-                info "requested-from-namespace $ns already exists in the namespaceMapping array. Skipping updating common-service-maps ConfigMap"
-                return 0
-            fi
-        done
-
-        current_control_ns=$(echo "$current_mapping" | awk '/controlNamespace:/ {print $2}')
-
-        # If controlNamespace is not set, assign the value of CONTROL_NS to it
-        if [ -z "$current_control_ns" ]; then
-            if [ -z "$CONTROL_NS" ]; then
-                error "MUST provide control namespace, controlNamespace is not set in common-service-maps ConfigMap"
-            else
-                info "controlNamespace not set in common-service-maps ConfigMap, setting to $CONTROL_NS"
-                current_mapping="controlNamespace: ${CONTROL_NS}\n$current_mapping"
-            fi
-        else
-            # Otherwise, if controlNamespace is set but different from CONTROL_NS, raise an error and abort the script
-            if [[ ! -z "$CONTROL_NS" && "$current_control_ns" != "$CONTROL_NS" ]]; then
-                error "controlNamespace is set to $current_control_ns but the script receives is $CONTROL_NS for --control-namespace"
-            fi
-        fi
-
-        # Update ConfigMap data
-        info "Updating common-service-maps ConfigMap in kube-public namespace"
-        NEW_MAPPING=$(echo -e "$current_mapping\n$NEW_MAPPING")
-
-        local object=$(
-            cat <<EOF
-apiVersion: v1
-kind: ConfigMap
-metadata:
-    name: common-service-maps
-    namespace: kube-public
-data:
-    common-service-maps.yaml: |
-$(echo "$NEW_MAPPING" | awk '{print "        "$0}')
-EOF
-)
-
-        echo "$object" | ${OC} apply -f -
-    else
-        # ConfigMap does not exist, create it
-        info "Creating common-service-maps ConfigMap in kube-public namespace"
-
-        NEW_MAPPING=$(echo -e "controlNamespace: $CONTROL_NS\nnamespaceMapping:\n$NEW_MAPPING")
-        local object=$(
-            cat <<EOF
-apiVersion: v1
-kind: ConfigMap
-metadata:
-    name: common-service-maps
-    namespace: kube-public
-data:
-    common-service-maps.yaml: |
-$(echo "$NEW_MAPPING" | awk '{print "        "$0}')
-EOF
-)
-        echo "$object" | ${OC} apply -f -
+    local isExists=$(echo "$current_yaml" | "${YQ}" '.namespaceMapping[] | select(.map-to-common-service-namespace == "'$map_to_cs_ns'")')
+    if [ -z "$isExists" ]; then
+        info "The provided map-to-common-service-namespace: $map_to_cs_ns, does not exist in common-service-maps"
+        info "Adding new map-to-commn-service-namespace"
+        updated_yaml=$(echo "$current_yaml" | "${YQ}" eval 'with(.namespaceMapping; . += [{"map-to-common-service-namespace": "'$map_to_cs_ns'"}])')
     fi
+
+    local tmp="\"$map_to_cs_ns\","
+
+    for ns in $namespaces; do
+        tmp="$tmp\"$ns\","
+    done
+    local ns_delimited="${tmp:0:-1}" # substring from 0 to length - 1
+
+    updated_yaml=$(echo "$updated_yaml" | "${YQ}" eval 'with(.namespaceMapping[]; select(.map-to-common-service-namespace == "'$map_to_cs_ns'").requested-from-namespace = ['$ns_delimited'])')
+    updated_yaml=$(echo "$updated_yaml" | "${YQ}" eval 'with(.namespaceMapping[]; select(.map-to-common-service-namespace == "'$map_to_cs_ns'").requested-from-namespace |= unique)')
+    local padded_yaml=$(echo "$updated_yaml" | awk '$0="    "$0')
+    update_cs_maps "$padded_yaml"
 }
 
 function msg() {
@@ -357,6 +145,18 @@ function info() {
     msg "[INFO] ${1}"
 }
 
-# --- Run ---
+# create_empty_csmaps
 
-main $*
+# insert_control_ns
+
+# update_tenant "tenant1"
+# echo "List of namespaces in tenant1: "
+# read_tenant_from_csmaps "tenant1"
+
+# update_tenant "tenant1" "ns1 ns2"
+# echo "List of namespaces in tenant1: "
+# read_tenant_from_csmaps "tenant1"
+
+# update_tenant "tenant2" "ns3"
+# echo "List of namespaces in tenant2: "
+# read_tenant_from_csmaps "tenant2"
