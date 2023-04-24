efa69c9faad4bdb94ccbbb20a5a0a62d07790863
Ben Luzarraga <31223504+bluzarraga@users.noreply.github.com>
2023-04-24 10:49:19 -0500

    Update Rollback script to be the reverse of the conversion script (#1161)
    * update backup restore and rollback with new conversion logic
    
    Signed-off-by: Ben Luzarraga <luzarragaben@ibm.com>
    
    * cleanup
    
    Signed-off-by: Ben Luzarraga <luzarragaben@ibm.com>
    
    * cleanup webhook and secretshare
    
    Signed-off-by: Ben Luzarraga <luzarragaben@ibm.com>
    
    * troubleshooting and cleanup
    
    Signed-off-by: Ben Luzarraga <luzarragaben@ibm.com>
    
    * extend retries to 10 minutes
    
    Signed-off-by: Ben Luzarraga <luzarragaben@ibm.com>
    
    * remove prototype check for iam requested
    
    Signed-off-by: Ben Luzarraga <luzarragaben@ibm.com>
    
    ---------
    
    Signed-off-by: Ben Luzarraga <luzarragaben@ibm.com>
    Co-authored-by: Ben Luzarraga <luzarragaben@ibm.com>

--- rollback-multi-instance.sh
+++ rollback-multi-instance.sh
@@ -19,35 +19,89 @@ set -o errexit
 set -o pipefail
 set -o errtrace
 set -o nounset
-MASTER_NS=$2
-CONTROL_NS=$1
-OC=${3:-oc}
-YQ=${3:-yq}
+MASTER_NS=
+CONTROL_NS=
+cm_name="common-service-maps"
+OC=oc
+YQ=yq
 
 
 function main() {
     which "${OC}" || error "Missing oc CLI"
     which "${YQ}" || error "Missing yq"
+    while [ "$#" -gt "0" ]
+    do
+        case "$1" in
+        "-h"|"--help")
+            usage
+            exit 0
+            ;;
+        "--original-cs-ns")
+            MASTER_NS=$2
+            shift
+            ;;
+        "--control-ns")
+            CONTROL_NS=$2
+            shift
+            ;;
+        *)
+            error "invalid option -- \`$1\`. Use the -h or --help option for usage info."
+            ;;
+        esac
+        shift
+    done
+    
     if [[ -z $CONTROL_NS ]]; then
         error "Control namespace not specified, please specify control namespace parameter and try again."
     fi
     if [[ -z $MASTER_NS ]]; then
-        error "Master common services namespace not specified, please specify common services namespace parameter and try again."
+        error "Original common services namespace not specified, please specify common services namespace parameter and try again."
     fi
+    collect_data
     rollback
+}
+
+function usage() {
+	local script="${0##*/}"
+
+	while read -r ; do echo "${REPLY}" ; done <<-EOF
+	Usage: ${script} [OPTION]...
+	Uninstall common services
+	Options:
+	Mandatory arguments to long options are mandatory for short options too.
+	  -h, --help                    display this help and exit
+      --original-cs-ns              specify the original common services namespace
+      --control-ns                  specify the existing control namespace
+	EOF
+}
+
+function collect_data() {
+    title "Collecting data"
+    msg "-----------------------------------------------------------------------"
+    
+    # info "MasterNS:${master_ns}"
+    # cs_operator_channel=$(${OC} get sub ibm-common-service-operator -n ${master_ns} -o yaml | yq ".spec.channel") 
+    # info "channel:${cs_operator_channel}"   
+    # catalog_source=$(${OC} get sub ibm-common-service-operator -n ${master_ns} -o yaml | yq ".spec.source")
+    # info "catalog_source:${catalog_source}" 
+    #this command gets all of the ns listed in requested from namesapce fields
+    requested_ns=$("${OC}" get configmap -n kube-public -o yaml ${cm_name} | yq '.data[]' | yq '.namespaceMapping[].requested-from-namespace' | awk '{print $2}' | tr '\n' ' ')
+    #this command gets all of the ns listed in map-to-common-service-namespace
+    map_to_cs_ns=$("${OC}" get configmap -n kube-public -o yaml ${cm_name} | yq '.data[]' | yq '.namespaceMapping[].map-to-common-service-namespace' | awk '{print}' | tr '\n' ' ')
+    if [[ $MASTER_NS != ${map_to_cs_ns%%[[:space:]]} ]]; then
+        error "The original common service namespace value entered does not match the value in the common-service-maps configmap. Make sure there is only one \"map-to-common-service-namesapce\" value specified in the configmap"
+    fi
 }
 
 function rollback() {
     info "Reverting multi-instance environment to shared instance environment."
 
     #checking if control namespace removed from common-service-maps
-    local cm_name="common-service-maps"
     return_value=$("${OC}" get configmap -n kube-public -o yaml ${cm_name} | yq '.data' | grep controlNamespace: > /dev/null || echo passed)
     if [[ $return_value != "passed" ]]; then
         error "Configmap: ${cm_name} still has controlNamespace field. This must be removed before proceeding with rollback."
     fi
     return_value="reset"
-    #TODO uninstall added instances
     
     info "Converting back to shared instance in ${MASTER_NS} namespace."
     # scale down
@@ -91,22 +145,21 @@ function rollback() {
     "${OC}" delete namespacescope common-service -n "${CONTROL_NS}" --ignore-not-found
     "${OC}" delete -n "${CONTROL_NS}" --ignore-not-found deploy ibm-namespace-scope-operator
 
-    #delete misc items in control namespace
-    "${OC}" delete deploy -n "${CONTROL_NS}" --ignore-not-found secretshare ibm-common-service-webhook
-    ${OC} delete svc ibm-common-service-webhook -n ${CONTROL_NS} --ignore-not-found
-    #restart pod in cs namespace to update webhook instance
-    webhookPod=$("${OC}" get pods -n ${MASTER_NS} | grep ibm-common-service-webhook | awk '{print $1}')
-    ${OC} delete pod ${webhookPod} -n ${MASTER_NS} --ignore-not-found
-    ${OC} delete deploy -n ${MASTER_NS} --ignore-not-found ibm-common-service-webhook
+    cleanup_webhook
+    cleanup_deployment "secretshare" "$CONTROL_NS"
 
     info "Deleting control namespace ${CONTROL_NS}"
     ${OC} delete namespace ${CONTROL_NS} --ignore-not-found
+
+    removeNSS
+    cleanupZenService
+    un_isolate_odlm "ibm-odlm" $MASTER_NS
 
     # scale back up
     scale_up_pod
 
     #verify singleton's are installed in master ns
-    retries=10
+    retries=20
     sleep_time=15
     total_time_mins=$(( sleep_time * retries / 60))
     info "Waiting for singleton services to deploy to ${MASTER_NS}..."
@@ -127,12 +180,37 @@ function rollback() {
             licPodCheck=$("${OC}" get pods -n "${CONTROL_NS}" --ignore-not-found | (grep ibm-licensing-service || echo "fail") | awk '{print $1}')
             if [ $certPodCheck != "fail" ] || [ $licPodCheck != "fail" ]; then
                 error "Singleton services re-deployed into control namespace. Verify that the common-services-map configmap in kube-public namespace has had the \"controlNamespace\" field removed and run again."
+            else
+                info "RETRYING: Waiting for singleton services to deploy to ${MASTER_NS}... (${retries} left)"
             fi
         fi
+        retries=$(( retries - 1 ))
     done
+
+    refresh_zen
+    refresh_kafka
 
     success "Cluster successfully rolled back from multi-instance to shared-instance."
 
+}
+
+function removeNSS(){
+    
+    title " Removing ODLM managed Namespace Scope CRs "
+    msg "-----------------------------------------------------------------------"
+
+    info "deleting namespace scope nss-managedby-odlm in namespace $MASTER_NS"
+    ${OC} delete nss nss-managedby-odlm -n $MASTER_NS --ignore-not-found || (error "unable to delete namespace scope nss-managedby-odlm in $MASTER_NS")
+    info "deleting namespace scope odlm-scope-managedby-odlm in namespace $MASTER_NS"
+    ${OC} delete nss odlm-scope-managedby-odlm -n $MASTER_NS --ignore-not-found || (error "unable to delete namespace scope odlm-scope-managedby-odlm in $MASTER_NS")
+    
+    info "deleting namespace scope nss-odlm-scope in namespace $MASTER_NS"
+    ${OC} delete nss nss-odlm-scope -n $MASTER_NS --ignore-not-found || (error "unable to delete namespace scope nss-odlm-scope in $MASTER_NS")
+    
+    info "deleting namespace scope common-service in namespace $MASTER_NS"
+    ${OC} delete nss common-service -n $MASTER_NS --ignore-not-found || (error "unable to delete namespace scope common-service in $MASTER_NS")
+
+    success "Namespace Scope CRs cleaned up"
 }
 
 function check_healthy() {
@@ -166,11 +244,255 @@ function check_healthy() {
     done
 }
 
+function cleanupZenService(){
+    title " Cleaning up Zen installation "
+    msg "-----------------------------------------------------------------------"
+    namespaces="$requested_ns $MASTER_NS"
+    for namespace in $namespaces
+    do
+        # remove cs namespace from zen service cr
+        return_value=$(${OC} get zenservice -n ${namespace} || echo "fail")
+        if [[ $return_value != "fail" ]]; then
+            if [[ $return_value != "" ]]; then
+                zenServiceCR=$(${OC} get zenservice -n ${namespace} | awk '{if (NR!=1) {print $1}}')
+                ${OC} patch zenservice ${zenServiceCR} -n ${namespace} --type json -p '[{ "op": "remove", "path": "/spec/csNamespace" }]' || info "CS Namespace not defined in ${zenServiceCR} in ${namespace}. Moving on..."
+            else
+                info "No zen service in namespace ${namespace}. Moving on..."
+            fi
+        else
+          info "Zen not installed in ${namespace}. Moving on..."
+        fi
+        return_value=""
+
+        # delete iam config job
+        return_value=$(${OC} get job -n ${namespace} | grep iam-config-job || echo "fail")
+        if [[ $return_value != "fail" ]]; then
+            ${OC} delete job iam-config-job -n ${namespace}
+        else
+            info "iam-config-job not present in namespace ${namespace}. Moving on..."
+        fi
+
+        # delete zen client
+        return_value=$(${OC} get client -n ${namespace} || echo "fail")
+        if [[ $return_value != "fail" ]]; then
+            if [[ $return_value != "" ]]; then
+                zenClient=$(${OC} get client -n ${namespace} | awk '{if (NR!=1) {print $1}}')
+                ${OC} patch client ${zenClient} -n ${namespace} --type=merge -p '{"metadata": {"finalizers":null}}'
+                ${OC} delete client ${zenClient} -n ${namespace}
+            else
+                info "No zen client in ${namespace}. Moving on..."
+            fi
+        else
+            info "Zen not installed in ${namespace}. Moving on..."
+        fi
+        return_value=""
+    done
+    
+    success "Zen instances cleaned up"
+}
+
+# wait for new cs to be ready
+function check_IAM(){
+    sleep 10
+
+    retries=40
+    sleep_time=30
+    total_time_mins=$(( sleep_time * retries / 60))
+    info "Waiting for IAM to come ready in namespace $MASTER_NS"
+    sleep 10
+    local cm="ibm-common-services-status"
+    local statusName="$MASTER_NS-iamstatus"
+    
+    while true; do
+        if [[ ${retries} -eq 0 ]]; then
+            error "Timeout after ${total_time_mins} minutes waiting for IAM to come ready in namespace $MASTER_NS"
+        fi
+
+        iamReady=$("${OC}" get configmap -n kube-public -o yaml ${cm} | (grep $statusName || echo fail))
+
+        if [[ "${iamReady}" == "fail" ]]; then
+            retries=$(( retries - 1 ))
+            info "RETRYING: Waiting for IAM service to be Ready (${retries} left)"
+            sleep ${sleep_time}
+        else
+            msg "-----------------------------------------------------------------------"    
+            success "IAM Service Ready in $MASTER_NS"
+            break
+        fi
+    done
+}
+
+# update zenservice CRs to be reconciled again
+function refresh_zen(){
+    title " Refreshing Zen Services "
+    msg "-----------------------------------------------------------------------"
+    #make sure IAM is ready before reconciling.
+    check_IAM #this will likely need to change in the future depending on how we check iam status
+    local namespaces="$requested_ns $MASTER_NS"
+    for namespace in $namespaces
+    do
+        return_value=$(${OC} get zenservice -n ${namespace} || echo "fail")
+        if [[ $return_value != "fail" ]]; then
+            if [[ $return_value != "" ]]; then
+                return_value=""
+                zenServiceCR=$(${OC} get zenservice -n ${namespace} | awk '{if (NR!=1) {print $1}}')
+                conversionField=$("${OC}" get zenservice ${zenServiceCR} -n ${namespace} -o yaml | yq '.spec | has("conversion")')
+                if [[ $conversionField == "false" ]]; then
+                    ${OC} patch zenservice ${zenServiceCR} -n ${namespace} --type='merge' -p '{"spec":{"conversion":"true"}}' || error "Zenservice ${zenServiceCR} in ${namespace} cannot be updated."
+                else
+                    ${OC} patch zenservice ${zenServiceCR} -n ${namespace} --type json -p '[{ "op": "remove", "path": "/spec/conversion" }]' || error "Zenservice ${zenServiceCR} in ${namespace} cannot be updated."
+                fi
+                conversionField=""
+            else
+                info "No zen service in namespace ${namespace}. Moving on..."
+            fi
+        else
+          info "Zen not installed in ${namespace}. Moving on..."
+        fi
+        return_value=""
+    done
+
+    success "Reconcile loop initiated for Zenservice instances"
+}
+
+function refresh_kafka () {
+    return_value=$(${OC} get kafkaclaim -A || echo fail)
+    if [[ $return_value != "fail" ]]; then
+        title " Refreshing Kafka Deployments "
+        msg "-----------------------------------------------------------------------"
+        local namespaces="$requested_ns $MASTER_NS"
+        for namespace in $namespaces
+        do
+            return_value=$(${OC} get kafkaclaim -n ${namespace} || echo "fail")
+            if [[ $return_value != "fail" ]]; then
+                if [[ $return_value != "" ]]; then
+                    kafkaClaims=$(${OC} get kafkaclaim -n ${namespace} | awk '{if (NR!=1) {print $1}}')
+                    #copy kc to file, delete original kc, re-apply copied file (check for an existing of the same name)
+                    for kc in $kafkaClaims
+                    do
+                        ${OC} get kafkaclaim -n ${namespace} $kc -o yaml > tmp.yaml
+                        ${OC} patch kafkaclaim ${kc} -n ${namespace} --type=merge -p '{"metadata": {"finalizers":null}}'
+                        ${OC} delete kafkaclaim ${kc} -n ${namespace} 
+                        ${OC} apply -f tmp.yaml  || info "kafkaclaim ${kc} already recreated. Moving on..."
+                    done
+                else
+                    info "No kafkaclaim in namespace ${namespace}. Moving on..."
+                fi
+            else
+            info "Kafka not installed in ${namespace}. Moving on..."
+            fi
+            return_value=""
+        done
+        
+        rm tmp.yaml -f
+        success "Reconcile loop initiated for Kafka instances"
+    else
+        info "Kafka not installed on cluster, no refresh needed."
+    fi
+}
+
 function scale_up_pod() {
     info "scaling back ibm-common-service-operator deployment in ${MASTER_NS} namespace"
     ${OC} scale deployment -n ${MASTER_NS} ibm-common-service-operator --replicas=1
     ${OC} scale deployment -n ${MASTER_NS} operand-deployment-lifecycle-manager --replicas=1
     check_healthy "${MASTER_NS}"
+}
+
+function un_isolate_odlm() {
+    package_name=$1
+    ns=$2
+    # get subscription of ODLM based on namespace 
+    sub_name=$(${OC} get subscription.operators.coreos.com -n ${ns} -l operators.coreos.com/${package_name}.${ns}='' --no-headers | awk '{print $1}')
+    if [ -z "$sub_name" ]; then
+        warning "Not found subscription ${package_name} in ${ns}"
+        return 0
+    fi
+    ${OC} get subscription.operators.coreos.com ${sub_name} -n ${ns} -o yaml > sub.yaml
+
+    # set ISOLATED_MODE to true
+    yq e '.spec.config.env |= (map(select(.name == "ISOLATED_MODE").value |= "false") + [{"name": "ISOLATED_MODE", "value": "false"}] | unique_by(.name))' sub.yaml -i
+
+    # apply updated subscription back to cluster
+    ${OC} apply -f sub.yaml
+    if [[ $? -ne 0 ]]; then
+        error "Failed to update subscription ${package_name} in ${ns}"
+    fi
+    rm sub.yaml
+
+    check_odlm_env "${ns}" 
+}
+
+function check_odlm_env() {
+    local namespace=$1
+    local name="operand-deployment-lifecycle-manager"
+    local condition="${OC} -n ${namespace} get deployment ${name} -o jsonpath='{.spec.template.spec.containers[0].env[?(@.name==\"ISOLATED_MODE\")].value}'| grep "false" || true"
+    local retries=10
+    local sleep_time=12
+    local total_time_mins=$(( sleep_time * retries / 60))
+    local wait_message="Waiting for OLM to update Deployment ${name} "
+    local success_message="Deployment ${name} is updated to not run in isolated mode"
+    local error_message="Timeout after ${total_time_mins} minutes waiting for OLM to update Deployment ${name} "
+
+    wait_for_condition "${condition}" ${retries} ${sleep_time} "${wait_message}" "${success_message}" "${error_message}"
+}
+
+function wait_for_condition() {
+    local condition=$1
+    local retries=$2
+    local sleep_time=$3
+    local wait_message=$4
+    local success_message=$5
+    local error_message=$6
+
+    info "${wait_message}"
+    while true; do
+        result=$(eval "${condition}")
+
+        if [[ ( ${retries} -eq 0 ) && ( -z "${result}" ) ]]; then
+            error "${error_message}"
+        fi
+
+        sleep ${sleep_time}
+        result=$(eval "${condition}")
+
+        if [[ -z "${result}" ]]; then
+            info "RETRYING: ${wait_message} (${retries} left)"
+            retries=$(( retries - 1 ))
+        else
+            break
+        fi
+    done
+
+    if [[ ! -z "${success_message}" ]]; then
+        success "${success_message}"
+    fi
+}
+
+function cleanup_deployment() {
+    local name=$1
+    local namespace=$2
+    info "Deleting existing Deployment ${name} in namespace ${namespace}..."
+    ${OC} delete deployment ${name} -n ${namespace} --ignore-not-found
+}
+
+function cleanup_webhook() {
+    podpreset_exist="true"
+    podpreset_exist=$(${OC} get podpresets.operator.ibm.com -n $CONTROL_NS --no-headers || echo "false")
+    if [[ $podpreset_exist != "false" ]] && [[ $podpreset_exist != "" ]]; then
+        echo $podpreset_exist
+        info "Deleting podpresets in namespace $CONTROL_NS..."
+        ${OC} get podpresets.operator.ibm.com -n $CONTROL_NS --no-headers --ignore-not-found | awk '{print $1}' | xargs ${OC} delete -n $CONTROL_NS --ignore-not-found podpresets.operator.ibm.com
+    fi
+
+    cleanup_deployment "ibm-common-service-webhook" $CONTROL_NS
+
+    info "Deleting MutatingWebhookConfiguration..."
+    ${OC} delete MutatingWebhookConfiguration ibm-common-service-webhook-configuration --ignore-not-found
+    ${OC} delete MutatingWebhookConfiguration ibm-operandrequest-webhook-configuration --ignore-not-found
+
+    info "Deleting ValidatingWebhookConfiguration..."
+    ${OC} delete ValidatingWebhookConfiguration ibm-cs-ns-mapping-webhook-configuration --ignore-not-found
+
 }
 
 function msg() {
