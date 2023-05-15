edc352dc5ae3f66cdfbd0a52227aef6105c4b95c
Ben Luzarraga <luzarragaben@gmail.com>
2023-05-15 13:11:50 -0500

    formatting
    Signed-off-by: Ben Luzarraga <luzarragaben@gmail.com>

--- isolate.sh
+++ isolate.sh
@@ -552,21 +552,23 @@ function check_if_certmanager_deployed() {
     done
 
     if [[ $deployed == "false" ]]; then
+        info "Cert manager not requested in scope, deploying..."
         cat <<EOF > tmp-opreq.yaml
 apiVersion: operator.ibm.com/v1alpha1
 kind: OperandRequest
 metadata:
-    labels:
-        app.kubernetes.io/instance: operand-deployment-lifecycle-manager
-        app.kubernetes.io/managed-by: operand-deployment-lifecycle-manager
-        app.kubernetes.io/name: odlm
-    name: ibm-cert-manager-operator
-    namespace: $namespace
+  labels:
+    app.kubernetes.io/instance: operand-deployment-lifecycle-manager
+    app.kubernetes.io/managed-by: operand-deployment-lifecycle-manager
+    app.kubernetes.io/name: odlm
+  name: ibm-cert-manager-operator
+  namespace: $namespace
 spec:
-requests:
-- operands:
-    - name: ibm-cert-manager-operator
-  registry: common-service
+  requests:
+    - operands:
+        - name: ibm-cert-manager-operator
+      registry: common-service
+      registryNamespace: $MASTER_NS
 EOF
 
     oc apply -f tmp-opreq.yaml
