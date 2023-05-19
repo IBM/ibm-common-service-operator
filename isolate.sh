715d34cea50746eca699f5587f437607d98eb190
Piotr Godowski <Piotr.Godowski@pl.ibm.com>
2023-05-19 14:25:03 +0200

    remove finalizer

--- isolate.sh
+++ isolate.sh
@@ -321,6 +321,7 @@ function uninstall_singletons() {
         "${OC}" delete -n "${MASTER_NS}" --ignore-not-found sub ibm-licensing-operator
         "${OC}" delete -n "${MASTER_NS}" --ignore-not-found csv "${csv}"
         "${OC}" delete -n "${MASTER_NS}" --ignore-not-found OperandBindInfo ibm-licensing-bindinfo
+        "${OC}" patch  -n "${MASTER_NS}" OperandBindInfo ibm-licensing-bindinfo --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
     fi
     "${OC}" delete -n "${MASTER_NS}" --ignore-not-found sub ibm-crossplane-operator-app
     "${OC}" delete -n "${MASTER_NS}" --ignore-not-found sub ibm-crossplane-provider-kubernetes-operator-app
