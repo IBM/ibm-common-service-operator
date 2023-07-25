870cdba7844cc511a4683df611af344a4c56022a
Henry Li <henry.h.li@ibm.com>
2023-07-25 17:11:12 -0400

    init checker_tool
    Signed-off-by: Henry Li <henry.h.li@ibm.com>

---
+++ checker_tool/check_cpfs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Licensed Materials - Property of IBM
+# Copyright IBM Corporation 2023. All Rights Reserved
+# US Government Users Restricted Rights -
+# Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+# This is an internal component, bundled with an official IBM product. 
+# Please refer to that particular license for additional information. 
+
+function main() {
+    echo "This is the main function"
+}
+
+main "$@"
