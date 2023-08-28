96f776b9ea25a59cb446eeca5d64f172d72986bb
Henry H Li <henry.h.li@ibm.com>
2023-08-28 18:30:56 -0400

    updated descriptions of CRD fields
    Signed-off-by: Henry H Li <henry.h.li@ibm.com>

--- api/v3/commonservice_types.go
+++ api/v3/commonservice_types.go
@@ -51,24 +51,50 @@ type ServiceConfig struct {
 
 // CommonServiceSpec defines the desired state of CommonService
 type CommonServiceSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	Features            *Features            `json:"features,omitempty"`
+	Features *Features `json:"features,omitempty"`
+	// InstallPlanApproval sets the approval mode for ODLM and other
+	// foundational services: Manual or Automatic
 	InstallPlanApproval olmv1alpha1.Approval `json:"installPlanApproval,omitempty"`
 	ManualManagement    bool                 `json:"manualManagement,omitempty"`
-	FipsEnabled         bool                 `json:"fipsEnabled,omitempty"`
-	RouteHost           string               `json:"routeHost,omitempty"`
-	Size                string               `json:"size,omitempty"`
-	Services            []ServiceConfig      `json:"services,omitempty"`
-	StorageClass        string               `json:"storageClass,omitempty"`
-	BYOCACertificate    bool                 `json:"BYOCACertificate,omitempty"`
-	ProfileController   string               `json:"profileController,omitempty"`
-	ServicesNamespace   ServicesNamespace    `json:"servicesNamespace,omitempty"`
-	OperatorNamespace   OperatorNamespace    `json:"operatorNamespace,omitempty"`
-	CatalogName         CatalogName          `json:"catalogName,omitempty"`
-	CatalogNamespace    CatalogNamespace     `json:"catalogNamespace,omitempty"`
-	DefaultAdminUser    string               `json:"defaultAdminUser,omitempty"`
+	// FipsEnabled enables FIPS mode for foundational services
+	FipsEnabled bool `json:"fipsEnabled,omitempty"`
+	// RouteHost describes the hostname for the foundational services route,
+	// and can only be configured pre-installation of IM
+	RouteHost string `json:"routeHost,omitempty"`
+	// Size describes the T-shirt size of foundational services: starterset,
+	// small, medium, or large
+	Size string `json:"size,omitempty"`
+	// Services describes the CPU, memory, and replica configuration for
+	// individual services in foundational services
+	Services []ServiceConfig `json:"services,omitempty"`
+	// StorageClass describes the storage class to use for the foundational
+	// services PVCs
+	StorageClass string `json:"storageClass,omitempty"`
+	// BYOCACertificate enables the option to replace the cs-ca-certificate with
+	// your own CA certificate
+	BYOCACertificate bool `json:"BYOCACertificate,omitempty"`
+	// ProfileController enables turbonomic to automatically handle sizing of
+	// foundational services. Default value is 'default'
+	ProfileController string `json:"profileController,omitempty"`
+	// ServicesNamespace describes the namespace where operands will be created
+	// in such as OperandRegistry and OperandConfig. This will also apply to all
+	// services in foundational services, e.g. IM will create operands in this
+	// namespace
+	ServicesNamespace ServicesNamespace `json:"servicesNamespace,omitempty"`
+	// OperatorNamespace describes the namespace where operators will be
+	// installed in such as ODLM. This will also apply to all services in
+	// foundational services, e.g. ODLM will install IM operator in this
+	// namespace
+	OperatorNamespace OperatorNamespace `json:"operatorNamespace,omitempty"`
+	// CatalogName is the name of the CatalogSource that will be used for ODLM
+	// and other services in foundational services
+	CatalogName CatalogName `json:"catalogName,omitempty"`
+	// CatalogNamespace is the namespace of the CatalogSource that will be used
+	// for ODLM and other services in foundational services
+	CatalogNamespace CatalogNamespace `json:"catalogNamespace,omitempty"`
+	// DefalutAdminUser is the name of the default admin user for foundational
+	// services IM, default is cpadmin
+	DefaultAdminUser string `json:"defaultAdminUser,omitempty"`
 
 	// +optional
 	License LicenseList `json:"license"`
@@ -108,7 +134,7 @@ type Bedrockshim struct {
 	CrossplaneProviderRemoval bool `json:"crossplaneProviderRemoval,omitempty"`
 }
 
-// BedrockOperator maintains a list of bedrock operators
+// BedrockOperator describes a list of foundational services' operators currently installed for this tenant.
 type BedrockOperator struct {
 	Name               string `json:"name,omitempty"`
 	Version            string `json:"version,omitempty"`
@@ -127,36 +153,63 @@ type CatalogName string
 type CatalogNamespace string
 
 type ConfigurableCR struct {
+	// ObjectName is the name of the configurable CommonService CR
 	ObjectName string `json:"objectName,omitempty"`
+	// ApiVersion is the api version of the configurable CommonService CR
 	APIVersion string `json:"apiVersion,omitempty"`
-	Namespace  string `json:"namespace,omitempty"`
-	Kind       string `json:"kind,omitempty"`
+	// Namespace is the namespace of the configurable CommonService CR
+	Namespace string `json:"namespace,omitempty"`
+	// Kind is the kind of the configurable CommonService CR
+	Kind string `json:"kind,omitempty"`
 }
 
+// ConfigStatus describes various configuration currently applied onto the foundational services installer.
 type ConfigStatus struct {
-	CatalogName             CatalogName       `json:"catalogName,omitempty"`
-	CatalogNamespace        CatalogNamespace  `json:"catalogNamespace,omitempty"`
-	OperatorNamespace       OperatorNamespace `json:"operatorNamespace,omitempty"`
-	OperatorDeployed        bool              `json:"operatorDeployed,omitempty"`
-	ServicesNamespace       ServicesNamespace `json:"servicesNamespace,omitempty"`
-	ServicesDeployed        bool              `json:"servicesDeployed,omitempty"`
-	Configurable            bool              `json:"configurable,omitempty"`
-	TopologyConfigurableCRs []ConfigurableCR  `json:"topologyConfigurableCRs,omitempty"`
+	// CatalogName is the name of the CatalogSource foundational services is using
+	CatalogName CatalogName `json:"catalogName,omitempty"`
+	// CatalogNamespace is the namesapce of the CatalogSource
+	CatalogNamespace CatalogNamespace `json:"catalogNamespace,omitempty"`
+	// OperatorNamespace is the namespace of where the foundational services'
+	// operators will be installed in.
+	OperatorNamespace OperatorNamespace `json:"operatorNamespace,omitempty"`
+	// OperatorDeployed indicates whether the OperandRegistry has been created
+	// or not.
+	OperatorDeployed bool `json:"operatorDeployed,omitempty"`
+	// ServicesNamespace is the namespace where the foundational services'
+	// operands will be created in.
+	ServicesNamespace ServicesNamespace `json:"servicesNamespace,omitempty"`
+	// ServicesDeployed indicates whether the OperandConfig has been created or
+	// not.
+	ServicesDeployed bool `json:"servicesDeployed,omitempty"`
+	// Configurable indicates whether this CommonService CR is the one that can
+	// that can be used to configure the foundational services' installer. Other
+	// CommonService CRs configurations will not take effect, except for sizing
+	Configurable bool `json:"configurable,omitempty"`
+	// TopologyConfigurableCRs describes the configurable CommonService CR
+	TopologyConfigurableCRs []ConfigurableCR `json:"topologyConfigurableCRs,omitempty"`
 }
 
 // CommonServiceStatus defines the observed state of CommonService
 type CommonServiceStatus struct {
+	// Phase describes the phase of the overall installation
 	Phase            string            `json:"phase,omitempty"`
 	BedrockOperators []BedrockOperator `json:"bedrockOperators,omitempty"`
-	OverallStatus    string            `json:"overallStatus,omitempty"`
-	ConfigStatus     ConfigStatus      `json:"configStatus,omitempty"`
+	// OverallStatus describes whether the Installation for the foundational services has succeeded or not
+	OverallStatus string       `json:"overallStatus,omitempty"`
+	ConfigStatus  ConfigStatus `json:"configStatus,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +operator-sdk:gen-csv:customresourcedefinitions.displayName="CommonService"
 
-// CommonService is the Schema for the commonservices API
+// CommonService is the Schema for the commonservices API. This API is used to
+// configure general foundational services configurations, such as sizing,
+// catalogsource, etc. See description of fields for more details. An instance
+// of this CRD is automatically created by the ibm-common-service-operator upon
+// installation to trigger the installation of critical installer components
+// such as operand-deployment-lifecycle-manager (ODLM), which is required to
+// further install other services from foundational services, such as IM.
 type CommonService struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
