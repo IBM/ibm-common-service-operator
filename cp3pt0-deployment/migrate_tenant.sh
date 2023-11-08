4d2563451118352db444fdad249294d0bf0d50c0
YuChen Shen <59578388+YCShen1010@users.noreply.github.com>
2023-11-08 13:32:56 -0500

    isolate LSR in ibm-common-services namespace (#1600)
    Signed-off-by: YuChen <yuchen.shen@mail.utoronto.ca>

--- cp3pt0-deployment/migrate_tenant.sh
+++ cp3pt0-deployment/migrate_tenant.sh
@@ -402,13 +402,13 @@ function isolate_license_service_reporter(){
         if [[ $return_value -gt 0 ]]; then
 
             # Change persistentVolumeReclaimPolicy to Retain
-            status=$("${OC}" get pvc license-service-reporter-pvc --ignore-not-found -n $OPERATOR_NS  --no-headers | awk '{print $2}' )
+            status=$("${OC}" get pvc license-service-reporter-pvc --ignore-not-found -n $SERVICES_NS  --no-headers | awk '{print $2}' )
             debug1 "LSR pvc status: $status"
             if [[ "$status" == "Bound" ]]; then
-                VOL=$("${OC}" get pvc license-service-reporter-pvc --ignore-not-found -n $OPERATOR_NS  -o=jsonpath='{.spec.volumeName}')
+                VOL=$("${OC}" get pvc license-service-reporter-pvc --ignore-not-found -n $SERVICES_NS  -o=jsonpath='{.spec.volumeName}')
                 debug1 "LSR volume name: $VOL"
                 if [[ -z "$VOL" ]]; then
-                    error "Volume for pvc license-service-reporter-pvc not found in $OPERATOR_NS"
+                    error "Volume for pvc license-service-reporter-pvc not found in $SERVICES_NS"
                 fi
 
                 # label LSR PV as LSR PV for further LSR upgrade
@@ -418,7 +418,7 @@ function isolate_license_service_reporter(){
                 ${OC} patch pv $VOL -p '{"spec": { "persistentVolumeReclaimPolicy" : "Retain" }}'
                 debug1 "License Service Reporter PV reclaim policy set to 'Retain'"
             else
-                info "No Lisense Service Reporter PVC found in $OPERATOR_NS or it is not in 'Bound' state, skipping isolation."
+                info "No Lisense Service Reporter PVC found in $SERVICES_NS or it is not in 'Bound' state, skipping isolation."
             fi
         fi
     fi
