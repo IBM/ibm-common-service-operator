353fbf8b31910753597b62c5f7327e8dc0300bd3
Ben Luzarraga <luzarragaben@gmail.com>
2024-02-20 11:48:02 -0600

    debugging
    Signed-off-by: Ben Luzarraga <luzarragaben@gmail.com>

--- velero/schedule/keycloak/keycloak-br-deploy.sh
+++ velero/schedule/keycloak/keycloak-br-deploy.sh
@@ -248,11 +248,10 @@ EOF
 
 function cleanup() {
   info "Clean up Keycloak BR resources..."
-  oc delete deploy keycloak-backup -n $KEYCLOAK_NAMESPACE --ignore-not-found && oc delete sa keycloak-backup-sa -n $KEYCLOAK_NAMESPACE --ignore-not-found && oc delete role keycloak-backup-role -n $KEYCLOAK_NAMESPACE --ignore-not-found && oc delete rolebinding keycloak-backup-rolebinding -n $KEYCLOAK_NAMESPACE --ignore-not-found && oc delete pvc keycloak-backup-pvc -n $KEYCLOAK_NAMESPACE --ignore-not-found
   if [[ $MODE == "restore" ]]; then
-    rs=$(oc get rs -n $KEYCLOAK_NAMESPACE --no-headers --ignore-not-found | grep keycloak-backup | awk '{print $1}')
-    oc delete rs $rs -n $KEYCLOAK_NAMESPACE --ignore-not-found
+    rs=$(oc get rs -n $KEYCLOAK_NAMESPACE --no-headers --ignore-not-found | grep keycloak-backup | awk '{print $1}' | tr "\n" " ")
   fi
+  oc delete deploy keycloak-backup -n $KEYCLOAK_NAMESPACE --ignore-not-found && oc delete rs $rs -n $KEYCLOAK_NAMESPACE --ignore-not-found && oc delete sa keycloak-backup-sa -n $KEYCLOAK_NAMESPACE --ignore-not-found && oc delete role keycloak-backup-role -n $KEYCLOAK_NAMESPACE --ignore-not-found && oc delete rolebinding keycloak-backup-rolebinding -n $KEYCLOAK_NAMESPACE --ignore-not-found && oc delete pvc keycloak-backup-pvc -n $KEYCLOAK_NAMESPACE --ignore-not-found
   success "Keycloak BR resources cleaned up."
 }
 
