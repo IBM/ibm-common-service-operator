05f8a372185c9caff59a5ffcff41d6b2c210b00c
Henry H Li <henry.h.li@ibm.com>
2024-04-02 00:06:19 -0400

    fixed missing license and copyright
    Signed-off-by: Henry H Li <henry.h.li@ibm.com>

--- controllers/operatorconfig.go
+++ controllers/operatorconfig.go
@@ -1,3 +1,19 @@
+//
+// Copyright 2024 IBM Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package controllers
 
 import (
@@ -6,12 +22,13 @@ import (
 	"fmt"
 	"strings"
 
-	v3 "github.com/IBM/ibm-common-service-operator/api/v3"
-	"github.com/IBM/ibm-common-service-operator/controllers/constant"
-	odlm "github.com/IBM/operand-deployment-lifecycle-manager/api/v1alpha1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/klog"
+
+	v3 "github.com/IBM/ibm-common-service-operator/api/v3"
+	"github.com/IBM/ibm-common-service-operator/controllers/constant"
+	odlm "github.com/IBM/operand-deployment-lifecycle-manager/api/v1alpha1"
 )
 
 func (r *CommonServiceReconciler) updateOperatorConfig(ctx context.Context, configList []v3.OperatorConfig) (bool, error) {
