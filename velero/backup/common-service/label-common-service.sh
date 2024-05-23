32aa0a33045f2e0899bc0e869a06fee8470827a8
YuChen <yuchen.shen@mail.utoronto.ca>
2024-05-23 10:20:01 -0700

    consolidate catalogsource label
    Signed-off-by: YuChen <yuchen.shen@mail.utoronto.ca>

--- velero/backup/common-service/label-common-service.sh
+++ velero/backup/common-service/label-common-service.sh
@@ -8,6 +8,7 @@
 # This is an internal component, bundled with an official IBM product.
 # Please refer to that particular license for additional information.
 
+set -o errtrace
 set -o nounset
 
 # ---------- Command arguments ----------
@@ -27,12 +28,8 @@ CS_SOURCE_NS="openshift-marketplace"
 CM_SOURCE_NS="openshift-marketplace"
 LIS_SOURCE_NS="openshift-marketplace"
 LSR_SOURCE_NS="openshift-marketplace"
-EDB_SOURCE_NS="openshift-marketplace"
-CS_SOURCE="opencloud-operators"
-CM_SOURCE="ibm-cert-manager-catalog"
-LIS_SOURCE="ibm-licensing-catalog"
-LSR_SOURCE="ibm-license-service-reporter-bundle-catalog"
-EDB_SOURCE="cloud-native-postgresql-catalog"
+
+ADDITIONAL_SOURCES=""
 
 # default values no change
 DEFAULT_SOURCE_NS="openshift-marketplace"
@@ -46,7 +43,7 @@ BASE_DIR=$(cd $(dirname "$0")/$(dirname "$(readlink $0)") && pwd -P)
 
 . ${BASE_DIR}/../../../cp3pt0-deployment/common/utils.sh
 
-source ${BASE_DIR}/local.properties
+source ${BASE_DIR}/env.properties
 
 function main() {
 
@@ -77,6 +74,7 @@ function pre_req(){
 }
 
 function label_catalogsource() {
+    ADDITIONAL_SOURCES=$(echo "$ADDITIONAL_SOURCES" | tr ',' ' ')
 
     title "Start to label the catalog sources... "
     # Label the Private CatalogSources in provided namespaces
@@ -85,29 +83,37 @@ function label_catalogsource() {
         CM_SOURCE_NS=$CERT_MANAGER_NAMESPACE
         LIS_SOURCE_NS=$LICENSING_NAMESPACE
         LSR_SOURCE_NS=$LSR_NAMESPACE
-        EDB_SOURCE_NS=$OPERATOR_NS
 
-        ${OC} label catalogsource ibm-operator-catalog foundationservices.cloudpak.ibm.com=catalog -n $DEFAULT_SOURCE_NS --overwrite=true 2>/dev/null 
-        ${OC} label catalogsource $CS_SOURCE foundationservices.cloudpak.ibm.com=catalog -n $CS_SOURCE_NS --overwrite=true 2>/dev/null 
-        ${OC} label catalogsource $CM_SOURCE foundationservices.cloudpak.ibm.com=catalog -n $CM_SOURCE_NS --overwrite=true 2>/dev/null 
-        ${OC} label catalogsource $LIS_SOURCE foundationservices.cloudpak.ibm.com=catalog -n $LIS_SOURCE_NS --overwrite=true 2>/dev/null 
-        ${OC} label catalogsource $EDB_SOURCE foundationservices.cloudpak.ibm.com=catalog -n $EDB_SOURCE_NS --overwrite=true 2>/dev/null
+        private_namespaces="$OPERATOR_NS,$CERT_MANAGER_NAMESPACE,$LICENSING_NAMESPACE,$LSR_NAMESPACE"
+        private_namespaces=$(echo "$private_namespaces" | tr ',' '\n')
+
+        while IFS= read -r namespace; do
+            label_ibm_catalogsources "$namespace"
+        done <<< "$private_namespaces"
     fi
 
-    # Label the CatalogSource with .spec.publisher having "IBM" in openshift-marketplace namespace
-    local ibm_catalog_sources=""
+    label_ibm_catalogsources "$DEFAULT_SOURCE_NS"
+}
+
+function label_ibm_catalogsources() {
+    local namespace=$1
+
+    # Label the CatalogSource with ".spec.publisher: IBM" in private namespace
+    local ibm_catalogsources=""
     while IFS=' ' read -r -a sources; do
         for source in "${sources[@]}"; do
-            if ${OC} get catalogsource "$source" -n "$DEFAULT_SOURCE_NS" -o json | grep -q '"publisher": *"IBM"*'; then
-                ibm_catalog_sources+=" $source"
+            if ${OC} get catalogsource "$source" -n "$namespace" -o json | grep -q '"publisher": *"IBM"*'; then
+                ibm_catalogsources+=" $source"
             fi
         done
-    done <<< "$(${OC} get catalogsource -n "$DEFAULT_SOURCE_NS" -o jsonpath='{.items[*].metadata.name}')"
-
-    # Remove leading/trailing whitespace
-    ibm_catalog_sources=$(echo "${ibm_catalog_sources}" | tr -s ' ' | sed 's/^ *//g' | sed 's/ *$//g')
-    for source in $ibm_catalog_sources; do
-        ${OC} label catalogsource "$source" foundationservices.cloudpak.ibm.com=catalog -n $DEFAULT_SOURCE_NS --overwrite=true 2>/dev/null
+    done <<< "$(${OC} get catalogsource -n "$namespace" -o jsonpath='{.items[*].metadata.name}')"
+    
+    # Add additional catalog sources
+    ibm_catalogsources="${ADDITIONAL_SOURCES}${ibm_catalogsources}"
+    # Remove leading and trailing spaces
+    ibm_catalogsources=$(echo "${ibm_catalogsources}" | tr -s ' ' | sed 's/^ *//g' | sed 's/ *$//g')
+    for source in $ibm_catalogsources; do
+         ${OC} label catalogsource "$source" foundationservices.cloudpak.ibm.com=catalog -n "$namespace" --overwrite=true 2>/dev/null
     done
 }
 
@@ -138,7 +144,8 @@ function label_ns_and_related() {
         done <<< "$operand_requests"
     done <<< "$namespaces"
 
-    ${OC} label secret pull-secret -n openshift-config foundationservices.cloudpak.ibm.com=pull-secret --overwrite=true
+    ${OC} label secret ibm-entitlement-key foundationservices.cloudpak.ibm.com=entitlementkey -n $DEFAULT_SOURCE_NS --overwrite=true 2>/dev/null
+    ${OC} label secret pull-secret -n openshift-config foundationservices.cloudpak.ibm.com=pull-secret --overwrite=true 2>/dev/null
 
 }
 
