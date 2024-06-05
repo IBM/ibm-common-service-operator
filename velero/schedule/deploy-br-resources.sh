d55383ab0219a24a683bf8f3684452f1a6b5d237
Ben Luzarraga <luzarragaben@gmail.com>
2024-06-05 15:17:50 -0500

    tweak deploy script
    Signed-off-by: Ben Luzarraga <luzarragaben@gmail.com>

--- velero/schedule/deploy-br-resources.sh
+++ velero/schedule/deploy-br-resources.sh
@@ -235,6 +235,7 @@ function deploy_resources(){
     sed -i -E "s/<operator namespace>/$OPERATOR_NAMESPACE/" ../spectrum-fusion/cpfs-util-resources/cpfs-util-br-configmap.yaml
     sed -i -E "s/<services or tethered namespace>/$TARGET_NAMESPACE/" ../spectrum-fusion/cpfs-util-resources/cpfs-util-serv-tethered-role.yaml
     sed -i -E "s/<services or tethered namespace>/$TARGET_NAMESPACE/" ../spectrum-fusion/cpfs-util-resources/cpfs-util-serv-tethered-rolebinding.yaml
+    sed -i -E "s/<operator namespace>/$OPERATOR_NAMESPACE/" ../spectrum-fusion/cpfs-util-resources/cpfs-util-serv-tethered-rolebinding.yaml
     if [[ $TETHERED_NS != "" ]]; then
       for ns in ${TETHERED_NS//,/ }; do
         sed -i -E "s/<services or tethered namespace>/$ns/" ../spectrum-fusion/cpfs-util-resources/cpfs-util-serv-tethered-role.yaml
