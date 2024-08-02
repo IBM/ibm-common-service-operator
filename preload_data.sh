adbf42b5f085c7ec760424e4e2931165e5b8f5b2
Ben Luzarraga <luzarragaben@gmail.com>
2024-08-02 10:11:39 -0500

    update logging, add ignore not found
    Signed-off-by: Ben Luzarraga <luzarragaben@gmail.com>

--- preload_data.sh
+++ preload_data.sh
@@ -51,6 +51,7 @@ function main() {
     trap cleanup_log EXIT
     prereq
     if [[ $RERUN == "true" ]]; then
+      info "Rerun specified..."
       deletemongocopy
     fi
     # run backup preload
@@ -1902,22 +1903,22 @@ function deletemongocopy {
   ${OC} delete cm namespace-scope --ignore-not-found
   
   #delete mongodump pvc and pv
-  VOL=$(${OC} get pvc cs-mongodump -o=jsonpath='{.spec.volumeName}')
+  VOL=$(${OC} get pvc cs-mongodump -o=jsonpath='{.spec.volumeName}' --ignore-not-found)
   if [[ -z "$VOL" ]]; then
     error "Volume for pvc cs-mongodump not found in $TO_NAMESPACE"
   fi
 
-  ${OC} patch pv $VOL -p '{"spec": { "persistentVolumeReclaimPolicy" : "Delete" }}'
+  ${OC} patch pv $VOL -p '{"spec": { "persistentVolumeReclaimPolicy" : "Delete" }}' --ignore-not-found
   
   ${OC} delete pvc cs-mongodump -n $TO_NAMESPACE --ignore-not-found --timeout=10s
   if [ $? -ne 0 ]; then
     info "Failed to delete pvc cs-mongodump, patching its finalizer to null..."
-    ${OC} patch pvc cs-mongodump -n $TO_NAMESPACE --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
+    ${OC} patch pvc cs-mongodump -n $TO_NAMESPACE --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]' --ignore-not-found
   fi
   ${OC} delete pv $VOL --ignore-not-found --timeout=10s
   if [ $? -ne 0 ]; then
     info "Failed to delete pv $VOL, patching its finalizer to null..."
-    ${OC} patch pv $VOL --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
+    ${OC} patch pv $VOL --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]' --ignore-not-found
   fi
 
   success "MongoDB restored to new namespace $TO_NAMESPACE"
