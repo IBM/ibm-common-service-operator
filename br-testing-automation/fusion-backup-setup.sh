389844f7e669b68efc30fcdf4ff6f2fc8c44754a
Ben Luzarraga <luzarragaben@gmail.com>
2024-08-08 12:11:02 -0500

    move wait to after applying file
    Signed-off-by: Ben Luzarraga <luzarragaben@gmail.com>

--- br-testing-automation/fusion-backup-setup.sh
+++ br-testing-automation/fusion-backup-setup.sh
@@ -148,12 +148,12 @@ function install_sf_br(){
         
         info "Connecting to spoke cluster $SPOKE_SERVER"
         #oc login to spoke cluster
-        ${OC} login --token=$SPOKE_OC_TOKEN --server=$SPOKE_SERVER
+        ${OC} login --token=$SPOKE_OC_TOKEN --server=$SPOKE_SERVER --insecure-skip-tls-verify=false
         ./cmd-line-install/install/install-isf-br.sh -s $catalog_image -n $SF_NAMESPACE || error "SF install script failed to install on spoke cluster."
         
         info "Connecting to hub cluster $HUB_SERVER"
         #oc login to the hub cluster
-        ${OC} login --token=$HUB_OC_TOKEN --server=$HUB_SERVER
+        ${OC} login --token=$HUB_OC_TOKEN --server=$HUB_SERVER --insecure-skip-tls-verify=false
         apiurl=$(oc whoami --show-server)
         cluster=$(echo $apiurl | cut -d":" -f2 | tr -d /)
         file=spokes_$cluster.yaml
@@ -163,16 +163,16 @@ function install_sf_br(){
         
         info "Re-connecting to spoke cluster $SPOKE_SERVER"
         #oc login to spoke cluster
-        ${OC} login --token=$SPOKE_OC_TOKEN --server=$SPOKE_SERVER
+        ${OC} login --token=$SPOKE_OC_TOKEN --server=$SPOKE_SERVER --insecure-skip-tls-verify=false
+        info "Applying spoke yaml..."
+        #apply generated yaml file
+        ${OC} apply -f $work_dir/$file || error "failed to apply spoke yaml."
         info "Waiting for BR Agent service to install on spoke cluster $SPOKE_SERVER..."
         while [[ $(${OC} get fusionserviceinstance ibm-backup-restore-agent-service-instance -n $SF_NAMESPACE -o jsonpath='{.status.installStatus.status}') != "Completed" ]]; do
             sleep 30
             progress=$(${OC} get fusionserviceinstance ibm-backup-restore-agent-service-instance -n $SF_NAMESPACE -o jsonpath='{.status.installStatus}')
             info "Install progress: $progress"
         done
-        info "Applying spoke yaml..."
-        #apply generated yaml file
-        ${OC} apply -f $work_dir/$file || error "failed to apply spoke yaml."
         
         info "Re-connecting to hub cluster $HUB_SERVER"
         #oc login to the hub cluster
