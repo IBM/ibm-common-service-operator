69832ec7bfb49bfbdd70957185a5244858303cca
Allen Li <46284272+qpdpQ@users.noreply.github.com>
2024-10-29 10:09:59 -0400

    conditionally wait for ibm-licensing-instance (#2252)
    * conditionally wait for ibm-licensing-instance
    
    Signed-off-by: Allen Li <liyuchen223@gmail.com>
    
    * check licensing deployment exist or not
    
    Signed-off-by: Allen Li <liyuchen223@gmail.com>
    
    * remove duplicate licensing deployment check
    
    Signed-off-by: Allen Li <liyuchen223@gmail.com>
    
    * add some log
    
    Signed-off-by: Allen Li <liyuchen223@gmail.com>
    
    ---------
    
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- cp3pt0-deployment/setup_singleton.sh
+++ cp3pt0-deployment/setup_singleton.sh
@@ -231,9 +231,11 @@ function is_migrate_licensing() {
     fi
 
     title "Check migrating LTSR ibm-licensing-operator"
-    wait_for_licensing_instance_deployment
-    local ns=$("$OC" get deployments -A | grep ibm-licensing-service-instance | cut -d ' ' -f1)
-    if [ -z "$ns" ]; then
+    # wait for ibm-licensing-operator instance 
+    local licensing_operator_exist=$("$OC" get deployment -A | (grep ibm-licensing-operator || echo "fail"))  
+    if [[ $licensing_operator_exist != "fail" ]]; then
+        wait_for_licensing_instance_deployment
+    else
         info "No LTSR ibm-licensing-operator to migrate, skipping"
         return 0
     fi
