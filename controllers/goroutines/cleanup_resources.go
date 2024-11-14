778618ba8c1a3f1f14a9b88b181e107c4b1e171c
Daniel Fan <fanyuchensx@gmail.com>
2024-11-13 23:52:14 -0500

    Clean MongoDB preload ConfigMap when the local MongoDB instance is owned by CR (#2302)
    * Clean MongoDB preload ConfigMap when the local MongoDB instance is owned by CR
    
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>
    
    * terminate iteration at the end of line
    
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>

--- controllers/goroutines/cleanup_resources.go
+++ controllers/goroutines/cleanup_resources.go
@@ -19,6 +19,9 @@ package goroutines
 import (
 	"time"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/klog"
 
 	"github.com/IBM/ibm-common-service-operator/v4/controllers/bootstrap"
@@ -26,8 +29,13 @@ import (
 	"github.com/IBM/ibm-common-service-operator/v4/controllers/constant"
 )
 
+const (
+	mongodbPreloadCm   = "mongodb-preload-endpoint"
+	mongodbStatefulSet = "icp-mongodb"
+)
+
 // Cleanup_Keycloak_Cert will delete Keycloak Certificate when OperandConfig is updated to new version
-func CleanupResources(bs *bootstrap.Bootstrap) {
+func CleanupKeycloakCert(bs *bootstrap.Bootstrap) {
 	for {
 		// wait ODLM OperandConfig CR resources
 		if err := bs.WaitResourceReady("operator.ibm.com/v1alpha1", "OperandConfig"); err != nil {
@@ -59,9 +67,9 @@ func CleanupResources(bs *bootstrap.Bootstrap) {
 					time.Sleep(5 * time.Second)
 					continue
 				}
-				if !exist && err == nil {
+				if !exist {
 					klog.Infof("cert-manager CRD does not exist, skip deleting Keycloak Certificate %s", constant.KeycloakCert)
-				} else if exist && err == nil {
+				} else if exist {
 					if err := bs.DeleteFromYaml(constant.KeycloakCertTemplate, bs.CSData); err != nil {
 						klog.Errorf("Failed to delete Keycloak Certificate %s: %v", constant.KeycloakCert, err)
 						time.Sleep(5 * time.Second)
@@ -73,4 +81,63 @@ func CleanupResources(bs *bootstrap.Bootstrap) {
 
 		time.Sleep(2 * time.Minute)
 	}
+}
+
+// Cleanup_MongoDB_Preload_CM will delete mongodb-preload-endpoint ConfigMap when icp-mongodb StatefulSet has owner reference
+func CleanupMongodbPreloadCm(bs *bootstrap.Bootstrap) {
+	for {
+		// check if icp-mongodb StatefulSet exists
+		statefulSet := &unstructured.Unstructured{
+			Object: map[string]interface{}{
+				"apiVersion": "apps/v1",
+				"kind":       "StatefulSet",
+				"metadata": map[string]interface{}{
+					"name":      mongodbStatefulSet,
+					"namespace": bs.CSData.ServicesNs,
+				},
+			},
+		}
+		if err := bs.Reader.Get(ctx, types.NamespacedName{Name: mongodbStatefulSet, Namespace: bs.CSData.ServicesNs}, statefulSet); err != nil {
+			if errors.IsNotFound(err) {
+				klog.Infof("StatefulSet %s does not exist in %s, skip deleting %s ConfigMap", mongodbStatefulSet, bs.CSData.ServicesNs, mongodbPreloadCm)
+				break
+			} else {
+				klog.Errorf("Failed to get StatefulSet %s: %v, retrying...", mongodbStatefulSet, err)
+				time.Sleep(5 * time.Second)
+				continue
+			}
+		}
+
+		// check if icp-mongodb StatefulSet has owner reference, delete mongodb-preload-endpoint ConfigMap
+		if (statefulSet.GetOwnerReferences() != nil) && (len(statefulSet.GetOwnerReferences()) > 0) {
+			preloadCm := &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"apiVersion": "v1",
+					"kind":       "ConfigMap",
+					"metadata": map[string]interface{}{
+						"name":      mongodbPreloadCm,
+						"namespace": bs.CSData.ServicesNs,
+					},
+				},
+			}
+			if err := bs.Client.Delete(ctx, preloadCm); err != nil {
+				if errors.IsNotFound(err) {
+					klog.Infof("ConfigMap %s does not exist in %s, skip deleting", mongodbPreloadCm, bs.CSData.ServicesNs)
+					break
+				}
+				klog.Errorf("Failed to delete ConfigMap %s: %v, retrying...", mongodbPreloadCm, err)
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			klog.Infof("ConfigMap %s in %s is deleted for the preparation of MongoDB migration.", mongodbPreloadCm, bs.CSData.ServicesNs)
+			break
+		}
+		klog.Infof("StatefulSet %s does not have owner reference, skip deleting %s ConfigMap", mongodbStatefulSet, mongodbPreloadCm)
+		break
+	}
+}
+
+func CleanupResources(bs *bootstrap.Bootstrap) {
+	go CleanupKeycloakCert(bs)
+	go CleanupMongodbPreloadCm(bs)
 }
