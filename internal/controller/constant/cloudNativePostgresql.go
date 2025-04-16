5317b701caf0fe9fa3a3a4d8400d1b5e798f05c2
YuChen Shen <59578388+YCShen1010@users.noreply.github.com>
2025-04-16 16:55:18 -0400

    update EDB operand images (#2493)
    Signed-off-by: YuChen <yuchen.shen@mail.utoronto.ca>

--- internal/controller/constant/cloudNativePostgresql.go
+++ internal/controller/constant/cloudNativePostgresql.go
@@ -27,10 +27,10 @@ metadata:
   annotations:
     version: {{ .Version }}
 data:
-  ibm-postgresql-17-operand-image: icr.io/cpopen/edb/postgresql:17.4@sha256:5f31883f560796827f8c64d43a06acb214e031d8931de63a0ccd56c3b27a9f36
-  ibm-postgresql-16-operand-image: icr.io/cpopen/edb/postgresql:16.8@sha256:43599de779d84195ffc7558541883545068aec300a2b745303327ea8b7b5aaf4
-  ibm-postgresql-15-operand-image: icr.io/cpopen/edb/postgresql:15.12@sha256:bfb664c8d6720e3ca19d698141a4188769e122d0535498cd522966195697dce0
-  ibm-postgresql-14-operand-image: icr.io/cpopen/edb/postgresql:14.17@sha256:f2a7f7cb13b7582dc629f2d484687573f88cc255f8af7849da630c7b4cfde4d0
-  ibm-postgresql-13-operand-image: icr.io/cpopen/edb/postgresql:13.20@sha256:adfa36eda97f9fbcefa23dd0e39dc4386380f33dbd26a5c2960e08854dc1af8d
+  ibm-postgresql-13-operand-image: icr.io/cpopen/edb/postgresql:13.20@sha256:4ccd786884e39641a74096d783742c219fb277caafe80b667a80396c3a9cf6c3
+  ibm-postgresql-14-operand-image: icr.io/cpopen/edb/postgresql:14.17@sha256:99589ee11b50af8ed6355a5f9c2272ce1fc7f9a13e80fda8eda5883a332782a3
+  ibm-postgresql-15-operand-image: icr.io/cpopen/edb/postgresql:15.12@sha256:0341c499ae144ee81dc2b92e8b99ee313ff6759374b7c1a6469156c20f522921
+  ibm-postgresql-16-operand-image: icr.io/cpopen/edb/postgresql:16.8@sha256:6d903fd4bd0ef3ef361d22b209ba7b686601b49d6f8d4e29b5d030da8d949dd5
+  ibm-postgresql-17-operand-image: icr.io/cpopen/edb/postgresql:17.4@sha256:c330a321438943fdd264b292b68fdfb8be89c3bde93a7bd987bd92c9dd2512b0
   edb-postgres-license-provider-image: cp.icr.io/cp/cpd/edb-postgres-license-provider@sha256:443b51f8b10acc85bdefde7193e0f45b1c423fa7fbdcaa28342805815c43db3d
 `
