7abc7939d02ec7a8df772d067782dd058e9663ab
YuChen <yuchen.shen@mail.utoronto.ca>
2025-05-01 20:49:53 -0700

    update back channel for co existence cases
    Signed-off-by: YuChen <yuchen.shen@mail.utoronto.ca>

--- internal/controller/constant/odlm.go
+++ internal/controller/constant/odlm.go
@@ -1114,31 +1114,59 @@ spec:
                 templatingValueFrom:
                   conditional:
                     expression:
-                      greaterThan:
-                        left:
-                          objectRef:
-                            apiVersion: apps/v1
-                            kind: Deployment
-                            name: rhbk-operator
-                            path: .metadata.labels.olm\.owner
-                        right:
-                          literal: rhbk-operator.v26.0.0
+                      and:
+                        - greaterThan:
+                            left:
+                              objectRef:
+                                apiVersion: apps/v1
+                                kind: Deployment
+                                name: rhbk-operator
+                                path: .metadata.labels.olm\.owner
+                            right:
+                              literal: rhbk-operator.v26.0.0
+                        - equal:
+                            left:
+                              objectRef:
+                                apiVersion: apiextensions.k8s.io/v1
+                                kind: CustomResourceDefinition
+                                name: keycloaks.k8s.keycloak.org
+                                path: .spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.hostname.properties.backchannelDynamic.type
+                            right:
+                              literal: "boolean"
                     then:
                       boolean: true
             additionalOptions:
               templatingValueFrom:
                 conditional:
                   expression:
-                    greaterThan:
-                      left:
-                        objectRef:
-                          apiVersion: apps/v1
-                          kind: Deployment
-                          name: rhbk-operator
-                          path: .metadata.labels.olm\.owner
-                      right:
-                        literal: rhbk-operator.v24.0.0
+                    and:
+                      - greaterThan:
+                          left:
+                            objectRef:
+                              apiVersion: apps/v1
+                              kind: Deployment
+                              name: rhbk-operator
+                              path: .metadata.labels.olm\.owner
+                          right:
+                            literal: rhbk-operator.v26.0.0
+                      - notEqual:
+                          left:
+                            objectRef:
+                              apiVersion: apiextensions.k8s.io/v1
+                              kind: CustomResourceDefinition
+                              name: keycloaks.k8s.keycloak.org
+                              path: .spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.hostname.properties.backchannelDynamic.type
+                          right:
+                            literal: "boolean"
                   then:
+                    array:
+                      - map:
+                          name: spi-user-profile-declarative-user-profile-config-file
+                          value: /mnt/user-profile/cs-keycloak-user-profile.json
+                      - map:
+                          name: hostname-backchannel-dynamic
+                          value: "true"
+                  else:
                     array:
                       - map:
                           name: spi-user-profile-declarative-user-profile-config-file
