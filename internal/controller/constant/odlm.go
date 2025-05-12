56f7850258a2a19bd2779cb46ac4a2858a4fdfe0
Allen Li <liyuchen223@gmail.com>
2025-05-12 17:24:20 -0400

    [SC2] add an new entry for event 5.2
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- internal/controller/constant/odlm.go
+++ internal/controller/constant/odlm.go
@@ -2049,6 +2049,12 @@ spec:
     packageName: ibm-events-operator
     scope: public
     installPlanApproval: {{ .ApprovalMode }}
+  - channel: v5.2
+    name: ibm-events-operator-v5.2
+    namespace: "{{ .CPFSNs }}"
+    packageName: ibm-events-operator
+    scope: public
+    installPlanApproval: {{ .ApprovalMode }}
   - name: ibm-platformui-operator
     namespace: "{{ .CPFSNs }}"
     channel: v4.4
