0a3ddb610245bd45ed4b45fe45a435de4257c2c2
Ben Luzarraga <luzarragaben@gmail.com>
2025-06-26 09:04:22 -0500

    remove storage location from singleton resources
    Signed-off-by: Ben Luzarraga <luzarragaben@gmail.com>

--- br-testing-automation/fusion-backup-setup.sh
+++ br-testing-automation/fusion-backup-setup.sh
@@ -276,7 +276,57 @@ function create_sf_resources(){
     cp ../velero/spectrum-fusion/backup_storage_location.yaml ./templates/backup_storage_location.yaml
     cp ../velero/spectrum-fusion/policy_assignment.yaml ./templates/policy_assignment.yaml
     cp ../velero/spectrum-fusion/policy.yaml ./templates/policy.yaml
-    recipe_name="cs-recipe"
+
+    info "Editing backup storage location resources..."
+    #backup storage secret
+    sed -i -E "s/<location name>/$BACKUP_STORAGE_LOCATION_NAME/" ./templates/backup_storage_location_secret.yaml
+    sed -i -E "s/<spectrum fusion ns>/$SF_NAMESPACE/" ./templates/backup_storage_location_secret.yaml
+    encoded_access_key=$(echo $STORAGE_SECRET_ACCESS_KEY | tr -d '\n' | base64 -w 0)
+    sed -i -E "s/<base 64 encoded secret access key>/$encoded_access_key/" ./templates/backup_storage_location_secret.yaml
+    encoded_access_key_id=$(echo $STORAGE_SECRET_ACCESS_KEY_ID | tr -d '\n' | base64 -w 0)
+    sed -i -E "s/<base 64 encoded access key id>/$encoded_access_key_id/" ./templates/backup_storage_location_secret.yaml
+    
+    #backup storage location
+    sed -i -E "s/<location name>/$BACKUP_STORAGE_LOCATION_NAME/" ./templates/backup_storage_location.yaml
+    sed -i -E "s/<spectrum fusion ns>/$SF_NAMESPACE/" ./templates/backup_storage_location.yaml
+    sed -i -E "s/<bucket name>/$STORAGE_BUCKET_NAME/" ./templates/backup_storage_location.yaml
+    #s3 url is breaking the sed command somehow, must be something in how the url is entered, maybe need to escape characters in the env.properties file
+    #escaping the : and // after https worked
+    sed -i -E "s/<s3 url>/$S3_URL/" ./templates/backup_storage_location.yaml
+    ${OC} apply -f ./templates/backup_storage_location_secret.yaml -f ./templates/backup_storage_location.yaml || error "Unable to create backup storage location resources to namespace $SF_NAMESPACE."
+    
+    #application
+    info "Editing application resource..."
+    sed -i -E "s/<operator namespace>/$OPERATOR_NS/" ./templates/application.yaml
+    sed -i -E "s/<service namespace>/$SERVICES_NS/" ./templates/application.yaml
+    sed -i -E "s/<tenant namespace 1>/$TETHERED_NAMESPACE1/" ./templates/application.yaml
+    sed -i -E "s/<tenant namespace 2>/$TETHERED_NAMESPACE2/" ./templates/application.yaml
+    sed -i -E "s/<cert manager namespace>/$CERT_MANAGER_NAMESPACE/" ./templates/application.yaml
+    sed -i -E "s/<licensing namespace>/$LICENSING_NAMESPACE/" ./templates/application.yaml
+    sed -i -E "s/<lsr namespace>/$LSR_NAMESPACE/" ./templates/application.yaml
+    sed -i -E "s/<spectrum fusion ns>/$SF_NAMESPACE/" ./templates/application.yaml
+
+    ${OC} apply -f ./templates/application.yaml || error "Unable to create application in namespace $SF_NAMESPACE."
+
+    #backup policy
+    info "Editing backup policy resource..."
+    sed -i -E "s/<storage_location>/$BACKUP_STORAGE_LOCATION_NAME/" ./templates/policy.yaml
+    ${OC} apply -f ./templates/policy.yaml -n $SF_NAMESPACE || error "Unable to create policy in namespace $SF_NAMESPACE."
+
+    #policyassignment
+    info "Editing policy assignment resource..."
+    sed -i -E "s/<spectrum fusion ns>/$SF_NAMESPACE/" ./templates/policy_assignment.yaml
+    if [[ $DYNAMIC == "true" ]]; then
+        recipe_name="cpfs-parent-recipe"
+        sed -i -E "s/<recipe namespace>/$OPERATOR_NS/" ./templates/policy_assignment.yaml
+        sed -i -E "s/<recipe name>/$recipe_name/" ./templates/policy_assignment.yaml
+    else
+        recipe_name="cs-recipe"
+        sed -i -E "s/<recipe namespace>/$SF_NAMESPACE/" ./templates/policy_assignment.yaml
+        sed -i -E "s/<recipe name>/$recipe_name/" ./templates/policy_assignment.yaml
+    fi
+    ${OC} apply -f ./templates/policy_assignment.yaml || error "Unable to create policy assignment in namespace $SF_NAMESPACE."
+
     if [[ $DYNAMIC == "true" ]]; then
         #core recipes
         cp ../velero/spectrum-fusion/recipes/dynamic-recipes/core/child-csdb-recipe-2.10.0.yaml ./templates/child-csdb-recipe.yaml
@@ -284,7 +334,6 @@ function create_sf_resources(){
         cp ../velero/spectrum-fusion/recipes/dynamic-recipes/core/child-nss-recipe.yaml ./templates/child-nss-recipe.yaml
         cp ../velero/spectrum-fusion/recipes/dynamic-recipes/core/parent-cpfs-recipe.yaml ./templates/parent-cpfs-recipe.yaml
         
-        recipe_name="cpfs-parent-recipe"
         info "Editing recipe resources..."
         #parent
         sed -i -E "s/<parent recipe namespace>/$OPERATOR_NS/" ./templates/parent-cpfs-recipe.yaml
@@ -382,54 +431,7 @@ function create_sf_resources(){
         fi
         ${OC} apply -f ./templates/multi-ns-recipe.yaml || error "Unable to create recipe in namespace $SF_NAMESPACE."
     fi
-
-    info "Editing backup storage location resources..."
-    #backup storage secret
-    sed -i -E "s/<location name>/$BACKUP_STORAGE_LOCATION_NAME/" ./templates/backup_storage_location_secret.yaml
-    sed -i -E "s/<spectrum fusion ns>/$SF_NAMESPACE/" ./templates/backup_storage_location_secret.yaml
-    encoded_access_key=$(echo $STORAGE_SECRET_ACCESS_KEY | tr -d '\n' | base64 -w 0)
-    sed -i -E "s/<base 64 encoded secret access key>/$encoded_access_key/" ./templates/backup_storage_location_secret.yaml
-    encoded_access_key_id=$(echo $STORAGE_SECRET_ACCESS_KEY_ID | tr -d '\n' | base64 -w 0)
-    sed -i -E "s/<base 64 encoded access key id>/$encoded_access_key_id/" ./templates/backup_storage_location_secret.yaml
     
-    #backup storage location
-    sed -i -E "s/<location name>/$BACKUP_STORAGE_LOCATION_NAME/" ./templates/backup_storage_location.yaml
-    sed -i -E "s/<spectrum fusion ns>/$SF_NAMESPACE/" ./templates/backup_storage_location.yaml
-    sed -i -E "s/<bucket name>/$STORAGE_BUCKET_NAME/" ./templates/backup_storage_location.yaml
-    #s3 url is breaking the sed command somehow, must be something in how the url is entered, maybe need to escape characters in the env.properties file
-    #escaping the : and // after https worked
-    sed -i -E "s/<s3 url>/$S3_URL/" ./templates/backup_storage_location.yaml
-    ${OC} apply -f ./templates/backup_storage_location_secret.yaml -f ./templates/backup_storage_location.yaml || error "Unable to create backup storage location resources to namespace $SF_NAMESPACE."
-    
-    #application
-    info "Editing application resource..."
-    sed -i -E "s/<operator namespace>/$OPERATOR_NS/" ./templates/application.yaml
-    sed -i -E "s/<service namespace>/$SERVICES_NS/" ./templates/application.yaml
-    sed -i -E "s/<tenant namespace 1>/$TETHERED_NAMESPACE1/" ./templates/application.yaml
-    sed -i -E "s/<tenant namespace 2>/$TETHERED_NAMESPACE2/" ./templates/application.yaml
-    sed -i -E "s/<cert manager namespace>/$CERT_MANAGER_NAMESPACE/" ./templates/application.yaml
-    sed -i -E "s/<licensing namespace>/$LICENSING_NAMESPACE/" ./templates/application.yaml
-    sed -i -E "s/<lsr namespace>/$LSR_NAMESPACE/" ./templates/application.yaml
-    sed -i -E "s/<spectrum fusion ns>/$SF_NAMESPACE/" ./templates/application.yaml
-
-    ${OC} apply -f ./templates/application.yaml || error "Unable to create application in namespace $SF_NAMESPACE."
-
-    #backup policy
-    info "Editing backup policy resource..."
-    sed -i -E "s/<storage_location>/$BACKUP_STORAGE_LOCATION_NAME/" ./templates/policy.yaml
-    ${OC} apply -f ./templates/policy.yaml -n $SF_NAMESPACE || error "Unable to create policy in namespace $SF_NAMESPACE."
-
-    #policyassignment
-    info "Editing policy assignment resource..."
-    sed -i -E "s/<spectrum fusion ns>/$SF_NAMESPACE/" ./templates/policy_assignment.yaml
-    sed -i -E "s/<recipe name>/$recipe_name/" ./templates/policy_assignment.yaml
-    if [[ $DYNAMIC == "true" ]]; then
-        sed -i -E "s/<recipe namespace>/$OPERATOR_NS/" ./templates/policy_assignment.yaml
-    else
-        sed -i -E "s/<recipe namespace>/$SF_NAMESPACE/" ./templates/policy_assignment.yaml
-    fi
-    ${OC} apply -f ./templates/policy_assignment.yaml || error "Unable to create policy assignment in namespace $SF_NAMESPACE."
-
     success "Spectrum Fusion BR resources created in namespace $SF_NAMESPACE."
 
 }
