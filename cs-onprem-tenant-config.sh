464576c8f87ae595086e409b03010c8eb90814ad
Raniprathyusha-Elaprolu <raniprathyusha.elaprolu@ibm.com>
2025-07-16 13:46:08 +0530

    Supporting export namespace variables externally and hostname change validation

--- cs-onprem-tenant-config.sh
+++ cs-onprem-tenant-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 configMapCustomHostname="cs-onprem-tenant-config"
-csNamespace=""
-map_to_common_service_namespace=""
+csNamespace=$operator_namespace
+map_to_common_service_namespace=$operand_namespace
 custom_hostname=""
 wlp_client_id=$(oc get secret -n $map_to_common_service_namespace platform-oidc-credentials -o jsonpath='{.data.WLP_CLIENT_ID}'|base64 -d)
 wlp_client_secret=$(oc get secret -n $map_to_common_service_namespace platform-oidc-credentials -o jsonpath='{.data.WLP_CLIENT_SECRET}'|base64 -d)
@@ -9,6 +9,15 @@ oauth2_client_registration_secret=$(oc get secret -n $map_to_common_service_name
 admin_password=$(oc get secret -n $map_to_common_service_namespace platform-auth-idp-credentials  -ojsonpath='{.data.admin_password}'|base64 -d)
 admin_username=$(oc get secret -n $map_to_common_service_namespace platform-auth-idp-credentials  -ojsonpath='{.data.admin_username}'|base64 -d)
 
+if [[ -z "$csNamespace" || -z "$map_to_common_service_namespace" ]]; then
+  echo "Error: One or both required environment variables are not set."
+  echo " Operator Namespace: '$csNamespace'"
+  echo " Operand Namespace: '$map_to_common_service_namespace'"
+  exit 1
+fi
+
+echo "Given operator namespace : $csNamespace"
+echo "Given operand namespace : $map_to_common_service_namespace"
 
 checkIfConfigMapExist(){
   count=$(oc get cm -n $map_to_common_service_namespace |grep $configMapCustomHostname |wc -l)
@@ -25,7 +34,7 @@ checkIfSecretExist(){
   if [[ "$count" -eq 1 ]]; then
     checkCrtFilesExist
     echo "Deleting old custom-tls-secret if exists"
-    oc delete secret $(oc get configmap -n $map_to_common_service_namespace cs-onprem-tenant-config -o jsonpath='{.data.custom_host_certificate_secret}') --ignore-not-found
+    oc delete secret $(oc get configmap -n $map_to_common_service_namespace cs-onprem-tenant-config -o jsonpath='{.data.custom_host_certificate_secret}') -n $map_to_common_service_namespace --ignore-not-found
     custom_secret=$(oc get configmap -n $map_to_common_service_namespace cs-onprem-tenant-config -o jsonpath='{.data.custom_host_certificate_secret}')
     echo "Creating custom-tls-secret"
     oc create secret generic $custom_secret -n $map_to_common_service_namespace --from-file=ca.crt=./ca.crt --from-file=tls.crt=./tls.crt --from-file=tls.key=./tls.key
@@ -131,6 +140,9 @@ check_job_completion() {
   oc wait job iam-custom-hostname --for condition=complete --timeout=120s
 }
 
+oc delete pod -l app=platform-auth-service -n $map_to_common_service_namespace
+echo "Triggered restart of platform-auth-service pods"
+
 deployment_name="platform-auth-service"
 timeout_seconds=180  # assuming auth-service will come in 3 mins after restart
 
@@ -156,3 +168,30 @@ done
 
 # Call the function to check job completion
 check_job_completion iam-custom-hostname $csNamespace
+
+#sleep for 10s and compare existing custom_hostname with the current route
+sleep 15
+
+username=$(oc get secret platform-auth-idp-credentials -n ibm-common-services -o json | jq -r .data.admin_username| base64 -d)
+password=$(oc get secret platform-auth-idp-credentials -n ibm-common-services -o json | jq -r .data.admin_password | base64 -d)
+route=$(oc get routes cp-console -o jsonpath='{.spec.host}')
+
+
+curl --silent -k -X POST -H "Content-Type: application/x-www-form-urlencoded;charset=UTF-8"  -d "grant_type=password&username=$username&password=$password&scope=openid" https://$route/idprovider/v1/auth/identitytoken > token_output
+if [ -f "token_output" ]; then
+  if grep -l '"access_token":"[^"]\+' token_output ; then
+      echo "Access token is present."
+      echo "Route changes validated successfully"
+  fi 
+  rm -f token_output
+else
+    echo "token_output does not exist."
+fi
+
+new_hostname=$(oc get routes cp-console --no-headers | awk '{print $2}')
+
+if [ "$new_hostname" = "$custom_hostname" ]; then
+    echo "successfully updated the custom hostname"
+else
+    echo "update custom hostname failed "
+fi
