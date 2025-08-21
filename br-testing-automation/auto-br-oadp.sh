59ec8c17cac9f9e2b2be3de78f7ef87abcc02458
Allen Li <liyuchen223@gmail.com>
2025-08-21 17:49:47 -0400

    add debug and preview_dir variable
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- br-testing-automation/auto-br-oadp.sh
+++ br-testing-automation/auto-br-oadp.sh
@@ -27,7 +27,6 @@ BASE_DIR=$(cd $(dirname "$0")/$(dirname "$(readlink $0)") && pwd -P)
 . ../cp3pt0-deployment/common/utils.sh
 PREVIEW_DIR=${BASE_DIR}
 
-
 function main() {
     parse_arguments "$@"
     prereq
