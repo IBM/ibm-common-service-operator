07d419aee31a95ce0365e75c296e4738a44048a5
Allen Li <liyuchen223@gmail.com>
2026-06-05 16:43:18 -0400

    check final map
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- internal/controller/operandconfig.go
+++ internal/controller/operandconfig.go
@@ -295,8 +295,8 @@ func mergeChangedMapWithExtremeSize(key string, defaultMap interface{}, changedM
 				defaultMapRef := defaultMap.([]interface{})
 				changedMapRef := changedMap.([]interface{})
 				for i := range changedMapRef {
-					// Check if defaultMapRef has enough elements to avoid index out of range panic
-					if i >= len(defaultMapRef) {
+					// Check bounds for both arrays before accessing
+					if i >= len(defaultMapRef) || i >= len(finalMap[key].([]interface{})) {
 						continue
 					}
 					for newKey := range changedMapRef[i].(map[string]interface{}) {
