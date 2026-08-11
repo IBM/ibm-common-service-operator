aa3a622c22746f5b558e5a33ddd2a0f92df17306
YCShen1010 <syc_libra@live.cn>
2026-08-11 09:13:34 -0700

    remove operator version suffix from postgresql image tags
    Signed-off-by: YCShen1010 <syc_libra@live.cn>

--- internal/controller/constant/cloudNativePostgresql.go
+++ internal/controller/constant/cloudNativePostgresql.go
@@ -28,10 +28,10 @@ metadata:
   annotations:
     version: {{ .Version }}
 data:
-  ibm-postgresql-14-operand-image: icr.io/cpopen/edb/postgresql:14.23-5.40.1@sha256:f67f5db05ba9d830d999f99248e7978812d498bd5f51f6cff3347b3672dd0580
-  ibm-postgresql-15-operand-image: icr.io/cpopen/edb/postgresql:15.18-5.40.1@sha256:60809f1ebc9df3c3aaed5aa2b8b6eb11402192c6cb949b4fbb4f4d622f300c19
-  ibm-postgresql-16-operand-image: icr.io/cpopen/edb/postgresql:16.14-5.40.1@sha256:97610e3b18dcdb90eda197481c2fb6b51bf4e4f39bebb97c1597b268c9dfefa4
-  ibm-postgresql-17-operand-image: icr.io/cpopen/edb/postgresql:17.10-5.40.1@sha256:1f566bbdc6feb07e818a06f680aaedaefdbb31a770562c27a2fe67ac2c970c44
-  ibm-postgresql-18-operand-image: icr.io/cpopen/edb/postgresql:18.4-5.40.1@sha256:7c2b8e6f1820fdb29523de45ba965d111fe0d959fc88e61dc88c6479ae67f880
+  ibm-postgresql-14-operand-image: icr.io/cpopen/edb/postgresql:14.23@sha256:f67f5db05ba9d830d999f99248e7978812d498bd5f51f6cff3347b3672dd0580
+  ibm-postgresql-15-operand-image: icr.io/cpopen/edb/postgresql:15.18@sha256:60809f1ebc9df3c3aaed5aa2b8b6eb11402192c6cb949b4fbb4f4d622f300c19
+  ibm-postgresql-16-operand-image: icr.io/cpopen/edb/postgresql:16.14@sha256:97610e3b18dcdb90eda197481c2fb6b51bf4e4f39bebb97c1597b268c9dfefa4
+  ibm-postgresql-17-operand-image: icr.io/cpopen/edb/postgresql:17.10@sha256:1f566bbdc6feb07e818a06f680aaedaefdbb31a770562c27a2fe67ac2c970c44
+  ibm-postgresql-18-operand-image: icr.io/cpopen/edb/postgresql:18.4@sha256:7c2b8e6f1820fdb29523de45ba965d111fe0d959fc88e61dc88c6479ae67f880
   edb-postgres-license-provider-image: cp.icr.io/cp/cpd/edb-postgres-license-provider@sha256:b839fa082ce65b218734d34e716119bb0c57ad0a761448c8b01153456f30728a
 `
