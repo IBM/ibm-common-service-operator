e60e3b235eed29fe8b3b9a303573d6c470841ca6
YCShen1010 <syc_libra@live.cn>
2026-08-20 09:40:20 -0700

    Add CSKeycloakThemeConfigMap constant for keycloak theme
    Signed-off-by: YCShen1010 <syc_libra@live.cn>

--- internal/controller/constant/constant.go
+++ internal/controller/constant/constant.go
@@ -96,9 +96,10 @@ const (
 	BedrockCatalogsourcePriority = "bedrock_catalogsource_priority"
 	// CSCACertificate is the name of cs-ca-certificate
 	CSCACertificate = "cs-ca-certificate"
+	// CSKeycloakThemeConfigMap is the name of cs-keycloak-theme
+	CSKeycloakThemeConfigMap = "cs-keycloak-theme"
 	// CSCACertificateSecret is the name of cs-ca-certificate-secret
 	CSCACertificateSecret = "cs-ca-certificate-secret"
-	// CertManagerSub is the name of ibm-cert-manager-operator subscription
 	CertManagerSub = "ibm-cert-manager-operator"
 	// DefaultImagePullSecret is the default name of the IBM entitlement key secret
 	DefaultImagePullSecret = "ibm-entitlement-key"
